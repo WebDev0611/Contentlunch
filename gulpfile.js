0de7045f6bf47c55b2421091ad3e535c5666e227
dach@surgeforward.com <unknown@kiln.example.com>
2014-02-22 06:27:14 +0800

    Fix gulpfile so that the watch command will work on Windows

--- gulpfile.js
+++ gulpfile.js
@@ -19,27 +19,27 @@ var gulp = require('gulp'),
 var gutil = require('gulp-util');
 
 gulp.task('styles', function() {
-	return gulp.src(['./bower_components/bootstrap/dist/css/bootstrap.css',
-					'./bower_components/bootstrap/dist/css/bootstrap-theme.css.css'])
-    .pipe(gulp.dest('public/assets/css'))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(minifycss())
-    .pipe(gulp.dest('public/assets/css'))
-    .pipe(livereload(server))
-    .pipe(notify({ message: 'Styles task complete' }));
+	return gulp.src([
+			'./bower_components/bootstrap/dist/css/bootstrap.css',
+			'./bower_components/bootstrap/dist/css/bootstrap-theme.css.css'
+		])
+		.pipe(gulp.dest('public/assets/css'))
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(minifycss())
+		.pipe(gulp.dest('public/assets/css'))
+		.pipe(livereload(server));
 });
 
 gulp.task('less', function () {
 	return gulp.src(['src/css/*.less'])
-	.pipe(less({
-		paths: [path.join(__dirname, 'less', 'includes')]
-	}))
-	.pipe(gulp.dest('public/assets/css'))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(minifycss())
-    .pipe(gulp.dest('public/assets/css'))
-    .pipe(livereload(server))
-    .pipe(notify({ message: 'LESS task complete' }));
+		.pipe(less({
+			paths: [path.join(__dirname, 'less', 'includes')]
+		}))
+		.pipe(gulp.dest('public/assets/css'))
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(minifycss())
+		.pipe(gulp.dest('public/assets/css'))
+		.pipe(livereload(server));
 });
 
 gulp.task('scripts', function() {
@@ -52,39 +52,34 @@ gulp.task('scripts', function() {
     .pipe(gulp.dest('./public/assets/js'));
 	return gulp.src(['src/js/app.js', 'src/js/**/*.js'])
 		.pipe(jshint('.jshintrc'))
-    .pipe(jshint.reporter('jshint-stylish'))
-    .pipe(concat('app.js'))
-    .pipe(gulp.dest('./public/assets/js'))
-    .pipe(livereload(server))
-    .pipe(notify({ message: 'Scripts task complete' }));
+		.pipe(jshint.reporter('jshint-stylish'))
+		.pipe(concat('app.js'))
+		.pipe(gulp.dest('./public/assets/js'))
+		.pipe(livereload(server));
 });
 
 gulp.task('fonts-eot', function () {
 	return gulp.src('./bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.eot')
 		.pipe(gulp.dest('./public/assets/fonts'))
-		.pipe(livereload(server))
-		.pipe(notify({ message: 'Font EOT task complete' }));
+		.pipe(livereload(server));
 });
 
 gulp.task('fonts-svg', function () {
 	return gulp.src('./bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.svg')
 		.pipe(gulp.dest('./public/assets/fonts'))
-		.pipe(livereload(server))
-		.pipe(notify({ message: 'Font SVG task complete' }));
+		.pipe(livereload(server));
 });
 
 gulp.task('fonts-ttf', function () {
 	return gulp.src('./bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.ttf')
 		.pipe(gulp.dest('./public/assets/fonts'))
-		.pipe(livereload(server))
-		.pipe(notify({ message: 'Font TTF task complete' }));
+		.pipe(livereload(server));
 });
 
 gulp.task('fonts-woff', function () {
 	return gulp.src('./bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff')
 		.pipe(gulp.dest('./public/assets/fonts'))
-		.pipe(livereload(server))
-		.pipe(notify({ message: 'Font WOFF task complete' }));
+		.pipe(livereload(server));
 });
 
 gulp.task('views', function () {
@@ -92,25 +87,18 @@ gulp.task('views', function () {
 	//	.pipe(embedlr())
 		.pipe(gulp.dest('public/assets/views'))
 		.pipe(livereload(server));
-		//.pipe(notify({ message: 'Views task complete' }));
 });
 
 gulp.task('images', function() {
 	return gulp.src('src/images/**/*')
-    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
-    .pipe(gulp.dest('public/assets/images'))
-    .pipe(livereload(server))
-    .pipe(notify({ message: 'Images task complete' }));
+		.pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+		.pipe(gulp.dest('public/assets/images'))
+		.pipe(livereload(server));
 });
 
 gulp.task('clean', function() {
 	return gulp.src(['public/assets/css', 'public/assets/js', 'public/assets/images', 'public/assets/views'], {read: false})
 		.pipe(clean());
-});
-
-// Run clean task first as dependency
-gulp.task('default', ['clean'], function() {
-	gulp.start('styles', 'less', 'scripts', 'views', 'images', 'fonts-eot', 'fonts-svg', 'fonts-ttf', 'fonts-woff')
 });
 
 gulp.task('watch', function() {
@@ -119,10 +107,19 @@ gulp.task('watch', function() {
 		if (err) {
 			return console.log(err);
 		}
-		gulp.watch('src/css/**/*.scss', ['styles']);
-		gulp.watch('src/css/**/*.less', ['less']);
-		gulp.watch('src/js/**/*.js', ['scripts']);
-		gulp.watch('src/views/**/*.html', ['views']);
-		gulp.watch('src/images/**/*', ['images']);
+		gulp.watch('src/css/**/*.css', ['styles']).on('change', function (e) { console.log('File "' + e.path + '" changed; CSS task complete'); });
+		gulp.watch('src/fonts/**/*.eot', ['fonts-eot']).on('change', function (e) { console.log('File "' + e.path + '" changed; FONT-EOT task complete'); });
+		gulp.watch('src/fonts/**/*.svg', ['fonts-svg']).on('change', function (e) { console.log('File "' + e.path + '" changed; FONT-SVG task complete'); });
+		gulp.watch('src/fonts/**/*.ttf', ['fonts-ttf']).on('change', function (e) { console.log('File "' + e.path + '" changed; FONT-TTF task complete'); });
+		gulp.watch('src/fonts/**/*.woff', ['fonts-woff']).on('change', function (e) { console.log('File "' + e.path + '" changed; FONT-WOFF task complete'); });
+		gulp.watch('src/css/**/*.less', ['less']).on('change', function (e) { console.log('File "' + e.path + '" changed; LESS task complete'); });
+		gulp.watch('src/js/**/*.js', ['scripts']).on('change', function (e) { console.log('File "' + e.path + '" changed; SCRIPTS task complete'); });
+		gulp.watch('src/views/**/*.html', ['views']).on('change', function (e) { console.log('File "' + e.path + '" changed; VIEW task complete'); });
+		gulp.watch('src/images/**/*', ['images']).on('change', function (e) { console.log('File "' + e.path + '" changed; IMAGE task complete'); });
 	});
+});
+
+// Run clean task first as dependency
+gulp.task('default', ['clean'], function () {
+	gulp.start('styles', 'less', 'scripts', 'views', 'images', 'fonts-eot', 'fonts-svg', 'fonts-ttf', 'fonts-woff');
 });
