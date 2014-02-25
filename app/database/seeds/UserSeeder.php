49dbc6002e464eb1919364ab7adb6fd22f9b6cec
Jason Kuchynka <jkuchynka@surgeforward.com>
2014-02-25 15:43:10 -0800

    Simpler login names

--- app/database/seeds/UserSeeder.php
+++ app/database/seeds/UserSeeder.php
@@ -6,20 +6,20 @@ class UserSeeder extends Seeder {
   {
     // Create users
     $users = array(
-      'jkuchynka+admin@surgeforward.com' => array('Admin'),
-      'jkuchynka+siteadmin@surgeforward.com' => array('Site Admin'),
-      'jkuchynka+contentcreator@surgeforward.com' => array('Content Creator'),
-      'jkuchynka+manager@surgeforward.com' => array('Manager'),
-      'jkuchynka+director@surgeforward.com' => array('Director'),
-      'jkuchynka+clevel@surgeforward.com' => array('C-Level'),
-      'jkuchynka+editor@surgeforward.com' => array('Editor'),
-      'jkuchynka+client@surgeforward.com' => array('Client'),
+      'admin' => array('Admin'),
+      'siteadmin' => array('Site Admin'),
+      'contentcreator' => array('Content Creator'),
+      'manager' => array('Manager'),
+      'director' => array('Director'),
+      'clevel' => array('C-Level'),
+      'editor' => array('Editor'),
+      'client' => array('Client'),
     );
     $now = date('Y-m-d H:i:s');
     $account = Account::where('title', '=', 'Surge')->first();
-    foreach ($users as $email => $roles) {
+    foreach ($users as $name => $roles) {
       $user = new User;
-      $user->username = $user->email = $email;
+      $user->username = $user->email = $name .'@test.com';
       $user->password = $user->password_confirmation = 'launch123';
       $user->created_at = $user->updated_at = time();
       $user->first_name = 'Test';
