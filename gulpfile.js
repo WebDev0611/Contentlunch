99b9c531fc87323fc00011e0e434beac1bdca7fb
dach@surgeforward.com <unknown@kiln.example.com>
2014-02-27 04:30:33 +0800

    Case #25935: Got admin and user popup menus working.

--- gulpfile.js
+++ gulpfile.js
@@ -57,6 +57,7 @@ gulp.task('less', function () {
 gulp.task('scripts', function() {
 	gulp.src([
 			'./bower_components/jquery/dist/jquery.js',
+			'./bower_components/bootstrap/dist/js/bootstrap.js',
 			'./bower_components/angular/angular.js',
 			'./bower_components/angular-route/angular-route.js',
 			'./bower_components/angular-resource/angular-resource.js',
--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-(function (window, angular) {
+(function(window, angular) {
 	'use strict';
 
 	launch = window.launch || (window.launch = { });
@@ -6,37 +6,39 @@
 	launch.module = angular.module('launch', ['ngRoute', 'ngResource', 'ngSanitize', 'ui.bootstrap']);
 
 	launch.module.config([
-		'$routeProvider', '$locationProvider', '$resourceProvider', function ($routeProvider, $locationProvider, $resource) {
-			$locationProvider.html5Mode(true);
-			$routeProvider
-				.when('/', {
-					controller: 'HomeController',
-					templateUrl: '/assets/views/home.html'
-				})
-				.when('/login', {
-					controller: 'LoginController',
-					templateUrl: '/assets/views/login.html'
-				})
-				.when('/accounts', {
-					controller: 'AccountsController',
-					templateUrl: '/assets/views/accounts.html'
-				})
-				.when('/user', {
-					controller: 'UserController',
-					templateUrl: '/assets/views/user.html'
-				})
-				.otherwise({
-					redirectTo: '/'
-				});
-		}
-	])
-	.run(['$rootScope', '$location', 'UserService', 'AuthService', function ($rootScope, $location, userService, authService) {
-		$rootScope.$on('$routeChangeStart', function (event, next, current) {
-			if ($location.path() === '/login') {
-				authService.logout();
-			} else if (!authService.isLoggedIn()) {
-				$location.path('/login');
+			'$routeProvider', '$locationProvider', '$resourceProvider', '$tooltipProvider', function($routeProvider, $locationProvider, $resource, $tooltipProvider) {
+				$locationProvider.html5Mode(true);
+				$routeProvider
+					.when('/', {
+						controller: 'HomeController',
+						templateUrl: '/assets/views/home.html'
+					})
+					.when('/login', {
+						controller: 'LoginController',
+						templateUrl: '/assets/views/login.html'
+					})
+					.when('/accounts', {
+						controller: 'AccountsController',
+						templateUrl: '/assets/views/accounts.html'
+					})
+					.when('/user', {
+						controller: 'UserController',
+						templateUrl: '/assets/views/user.html'
+					})
+					.otherwise({
+						redirectTo: '/'
+					});
 			}
-		});
-	}]);
+		])
+		.run([
+			'$rootScope', '$location', 'UserService', 'AuthService', function($rootScope, $location, userService, authService) {
+				$rootScope.$on('$routeChangeStart', function(event, next, current) {
+					if ($location.path() === '/login') {
+						authService.logout();
+					} else if (!authService.isLoggedIn()) {
+						$location.path('/login');
+					}
+				});
+			}
+		]);
 })(window, angular);
