7699fd246974e15bb7c6350d2651d98cb3944407
dach@surgeforward.com <unknown@kiln.example.com>
2014-02-28 03:47:36 +0800

    Case #26150: Create client model for Users and began expanding the User service to map DTOs to the client User model. Also started on the basic layout for the user management page.

--- src/js/controllers/users-controller.js
+++ src/js/controllers/users-controller.js
@@ -1,9 +1,8 @@
 launch.module.controller('UsersController', [
-	'$scope', '$location', 'UserService', function ($scope, $location, UserService) {
+	'$scope', '$location', 'UserService', function($scope, $location, UserService) {
 		$scope.title = 'This is the users page controller';
-    $scope.users = [];
-    UserService.query(function (users) {
-      $scope.users = users;
-    });
+		$scope.users = [];
+
+		$scope.users = UserService.get($scope.users);
 	}
 ]);
--- src/js/services/user-service.js
+++ src/js/services/user-service.js
@@ -1,4 +1,83 @@
 
 launch.module.factory('UserService', function ($resource) {
-  return $resource('/api/user/:id', { id: '@id' });
+	var map = {
+		parseResponse: function(r, a, b, c) {
+			var dto = JSON.parse(r);
+
+			if ($.isArray(dto)) {
+				var users = [];
+
+				angular.forEach(dto, function (user, index) {
+					users.push(map.fromDto(user));
+				});
+
+				return users;
+			}
+
+			if ($.isObject(dto)) {
+				return map.fromDto(dto);
+			}
+
+			return null;
+		},
+		fromDto: function (dto) {
+			var user = new User();
+
+			user.id = dto.id;
+			user.userName = dto.userName;
+			user.firstName = dto.first_name;
+			user.lastName = dto.last_name;
+			user.email = dto.email;
+			user.created = dto.created_at;
+			user.updated = dto.updated_at;
+			user.confirmed = dto.confirmed;
+
+			return user;
+		},
+		toDto: function (user) {
+			return {
+				id: user.id,
+				userName: user.userName,
+				first_name: user.firstName,
+				last_name: user.lastName,
+				email: user.email,
+				created_at: user.created,
+				updated_at: user.updated,
+				confirmed: user.confirmed
+			};
+		}
+	};
+
+	var resource = $resource('/api/user/:id', { id: '@id' }, {
+		query: { method: 'GET', isArray: true, transformResponse: map.parseResponse }
+	});
+
+	var User = function () {
+		var self = this;
+
+		self.formatName = function() {
+			if (!launch.utils.isBlank(self.firstName) && !launch.utils.isBlank(self.lastName)) {
+				return self.firstName + ' ' + self.lastName;
+			}
+
+			if (!launch.utils.isBlank(self.userName)) {
+				return self.userName;
+			}
+
+			if (!launch.utils.isBlank(self.email)) {
+				return self.email;
+			}
+
+			return self.id;
+		};
+
+		return self;
+	};
+
+	return {
+		query: function(params) { },
+		get: function () {
+			return resource.query();
+		}
+	};
 });
