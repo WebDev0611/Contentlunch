649973b5a1e0feaa9378b9b25efd1a5a7103e176
dach@surgeforward.com <unknown@kiln.example.com>
2014-03-03 04:27:09 +0800

    Case #26150: Got filtering tied up with pagination.

--- src/js/controllers/users-controller.js
+++ src/js/controllers/users-controller.js
@@ -1,14 +1,14 @@
 launch.module.controller('UsersController', [
 	'$scope', '$location', '$filter', 'UserService', function ($scope, $location, $filter, UserService) {
-		$scope.title = 'This is the users page controller';
 		$scope.users = [];
 		$scope.filteredUsers = [];
+		$scope.pagedUsers = [];
 
 		$scope.search = {
 			searchTerm: null,
 			searchTermMinLength: 1,
 			userStatus: 'active',
-			toggleStatus: function(status) {
+			toggleStatus: function (status) {
 				this.userStatus = status;
 			}
 		};
@@ -17,7 +17,7 @@ launch.module.controller('UsersController', [
 			totalItems: 0,
 			pageSize: 2,
 			currentPage: 1,
-			currentSort: null,
+			currentSort: 'firstName',
 			currentSortDirection: 'ASC',
 			onPageChange: function (page) {
 				// IF WE WANT TO PAGE FROM THE SERVER, ENTER THAT CODE AND
@@ -32,21 +32,34 @@ launch.module.controller('UsersController', [
 
 		$scope.users = UserService.query(null, {
 			success: function (users) {
-				$scope.pagination.totalItems = $scope.users.length;
+				$scope.applyFilter();
 			}
 		});
 
-		$scope.applyFilter = function (user) {
-			if (!launch.utils.isBlank($scope.search.searchTerm) && $scope.search.searchTerm.length >= $scope.search.searchTermMinLength) {
-				return  (launch.utils.isBlank($scope.search.searchTerm) ? true : user.matchSearchTerm($scope.search.searchTerm));
-			}
+		$scope.applyFilter = function () {
+			$scope.filteredUsers = $filter('filter')($scope.users, function(user) {
+				if (!launch.utils.isBlank($scope.search.searchTerm) && $scope.search.searchTerm.length >= $scope.search.searchTermMinLength) {
+					return (launch.utils.isBlank($scope.search.searchTerm) ? true : user.matchSearchTerm($scope.search.searchTerm));
+				}
 
-			return true;
+				return true;
+			});
+
+			$scope.pagination.currentPage = 1;
+			$scope.pagination.totalItems = $scope.filteredUsers.length;
+			$scope.groupToPages();
 		};
 
-		$scope.$watch('search.searchTerm', function () {
-			if (!launch.utils.isBlank($scope.search.searchTerm) && $scope.search.searchTerm.length >= $scope.search.searchTermMinLength) {
+		$scope.groupToPages = function() {
+			$scope.pagedUsers = [];
+
+			for (var i = 0; i < $scope.filteredUsers.length; i++) {
+				if (i % $scope.pagination.pageSize === 0) {
+					$scope.pagedUsers[Math.floor(i / $scope.pagination.pageSize)] = [$scope.filteredUsers[i]];
+				} else {
+					$scope.pagedUsers[Math.floor(i / $scope.pagination.pageSize)].push($scope.filteredUsers[i]);
+				}
 			}
-		}, true);
+		};
 	}
 ]);
