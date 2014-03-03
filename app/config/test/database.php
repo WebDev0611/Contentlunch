dd905f010a389a92287dc33e51cc6747003e67e0
Jason Kuchynka <jkuchynka@surgeforward.com>
2014-03-03 13:16:53 -0800

    Case #26240
    Add test server configs

---
+++ app/config/test/database.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+	'default' => 'mysql',
+	'connections' => array(
+		'mysql' => array(
+      'driver'    => 'mysql',
+      'host'      => '10.254.3.25',
+      'database'  => 'contentlaunch',
+      'username'  => 'contentlaunch',
+      'password'  => 'QmnZhnaDZtM3tsmYkmU2',
+      'charset'   => 'utf8',
+      'collation' => 'utf8_unicode_ci',
+      'prefix'    => '',
+		),
+	)
+);
