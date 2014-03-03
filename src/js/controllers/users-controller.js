7c26dc5a1b89ed42038791fa03b9b32e4063403e
dach@surgeforward.com <unknown@kiln.example.com>
2014-03-03 09:06:43 +0800

    Case #26150: Created a basic user edit for and hooked up the user's info to it when selected.

--- src/js/controllers/users-controller.js
+++ src/js/controllers/users-controller.js
@@ -3,13 +3,27 @@ launch.module.controller('UsersController', [
 		$scope.users = [];
 		$scope.filteredUsers = [];
 		$scope.pagedUsers = [];
+		$scope.selectedUser = null;
 
 		$scope.search = {
 			searchTerm: null,
 			searchTermMinLength: 1,
 			userStatus: 'active',
-			toggleStatus: function (status) {
+			toggleStatus: function(status) {
 				this.userStatus = status;
+			},
+			applyFilter: function() {
+				$scope.filteredUsers = $filter('filter')($scope.users, function(user) {
+					if (!launch.utils.isBlank($scope.search.searchTerm) && $scope.search.searchTerm.length >= $scope.search.searchTermMinLength) {
+						return (launch.utils.isBlank($scope.search.searchTerm) ? true : user.matchSearchTerm($scope.search.searchTerm));
+					}
+
+					return true;
+				});
+
+				$scope.pagination.currentPage = 1;
+				$scope.pagination.totalItems = $scope.filteredUsers.length;
+				$scope.pagination.groupToPages();
 			}
 		};
 
@@ -19,47 +33,62 @@ launch.module.controller('UsersController', [
 			currentPage: 1,
 			currentSort: 'firstName',
 			currentSortDirection: 'ASC',
-			onPageChange: function (page) {
+			onPageChange: function(page) {
 				// IF WE WANT TO PAGE FROM THE SERVER, ENTER THAT CODE AND
 				// REMOVE THE getPagedUsers FUNCTION BELOW. ALSO, WE'LL NEED
 				// TO TWEAK THE WHAT THAT pagination.totalItems IS CALCULATED
 				// SUCH THAT THIS VALUE COMES BACK IN THE JSON RESPONSE.
 			},
-			showPager: function () {
+			showPager: function() {
 				return (this.totalItems > this.pageSize);
+			},
+			groupToPages: function() {
+				$scope.pagedUsers = [];
+
+				for (var i = 0; i < $scope.filteredUsers.length; i++) {
+					if (i % $scope.pagination.pageSize === 0) {
+						$scope.pagedUsers[Math.floor(i / $scope.pagination.pageSize)] = [$scope.filteredUsers[i]];
+					} else {
+						$scope.pagedUsers[Math.floor(i / $scope.pagination.pageSize)].push($scope.filteredUsers[i]);
+					}
+				}
 			}
 		};
 
 		$scope.users = UserService.query(null, {
 			success: function (users) {
-				$scope.applyFilter();
+				$scope.search.applyFilter();
 			}
 		});
 
-		$scope.applyFilter = function () {
-			$scope.filteredUsers = $filter('filter')($scope.users, function(user) {
-				if (!launch.utils.isBlank($scope.search.searchTerm) && $scope.search.searchTerm.length >= $scope.search.searchTermMinLength) {
-					return (launch.utils.isBlank($scope.search.searchTerm) ? true : user.matchSearchTerm($scope.search.searchTerm));
-				}
-
-				return true;
-			});
-
-			$scope.pagination.currentPage = 1;
-			$scope.pagination.totalItems = $scope.filteredUsers.length;
-			$scope.groupToPages();
+		$scope.isSelectedUser = function(user) {
+			return user === $scope.selectedUser;
 		};
 
-		$scope.groupToPages = function() {
-			$scope.pagedUsers = [];
+		$scope.enterNewUser = function() {
+			$scope.selectedUser = UserService.getNewUser();
+		};
 
-			for (var i = 0; i < $scope.filteredUsers.length; i++) {
-				if (i % $scope.pagination.pageSize === 0) {
-					$scope.pagedUsers[Math.floor(i / $scope.pagination.pageSize)] = [$scope.filteredUsers[i]];
-				} else {
-					$scope.pagedUsers[Math.floor(i / $scope.pagination.pageSize)].push($scope.filteredUsers[i]);
-				}
+		$scope.selectUser = function(user) {
+			if ($scope.selectedUser === user) {
+				$scope.selectedUser = null;
+			} else {
+				$scope.selectedUser = user;
 			}
+		};
+
+		$scope.cancelEdit = function () {
+			$scope.selectedUser = null;
+
+			$scope.users = UserService.query(null, {
+				success: function (users) {
+					$scope.search.applyFilter();
+				}
+			});
+		};
+
+		$scope.saveUser = function () {
+
 		};
 	}
 ]);
