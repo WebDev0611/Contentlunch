8ca17eb1694c7afdbca68ec5bfe54942c6088911
Jason Kuchynka <jkuchynka@surgeforward.com>
2014-03-25 10:35:07 -0700

    Remove Envoy, add custom deploy command

---
+++ app/commands/DeployCommand.php
@@ -0,0 +1,78 @@
+<?php
+
+use Illuminate\Console\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+
+class DeployCommand extends Command {
+
+	/**
+	 * The console command name.
+	 *
+	 * @var string
+	 */
+	protected $name = 'deploy:test';
+
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Deploy to test server.';
+
+	/**
+	 * Create a new command instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	/**
+	 * Execute the console command.
+	 *
+	 * @return mixed
+	 */
+	public function fire()
+	{
+    $token = 'qoOqob0jl9uXbXkkcaClq8wAdlabxNndQfm96LS';
+    $room = 3091332;
+//    $hipchat = new HipChat\HipChat($token);
+		SSH::run(array(
+      'cd /www/contentlaunch',
+      'git pull origin master',
+      'npm update',
+      'php composer.phar update',
+      'bower update',
+      'php artisan migrate --env="test"',
+      //'phpunit',
+      'gulp',
+  //    $hc->message_room($room, 'Test Deploy', 'Deployed changes to test server')
+    ));
+	}
+
+	/**
+	 * Get the console command arguments.
+	 *
+	 * @return array
+	 */
+	protected function getArguments()
+	{
+		return array(
+		);
+	}
+
+	/**
+	 * Get the console command options.
+	 *
+	 * @return array
+	 */
+	protected function getOptions()
+	{
+		return array(
+		);
+	}
+
+}
