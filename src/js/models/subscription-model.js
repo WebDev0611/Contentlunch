1b7fc205b92e9b5cea1c1ef505bc5aa1cfc8867f
dach@surgeforward.com <unknown@kiln.example.com>
2014-03-27 02:44:52 +0700

    Case #26516: Fix problems with validating a subscription for a new account.

--- src/js/models/subscription-model.js
+++ src/js/models/subscription-model.js
@@ -16,14 +16,7 @@
 	self.created = null;
 	self.updated = null;
 
-	self.components = [
-		{ name: 'create', title: 'CREATE', active: true },
-		{ name: 'calendar', title: 'CALENDAR', active: true },
-		{ name: 'launch', title: 'LAUNCH', active: true },
-		{ name: 'measure', title: 'MEASURE', active: true },
-		{ name: 'collaborate', title: 'COLLABORATE', active: self.subscriptionLevel >= 2 },
-		{ name: 'consult', title: 'CONSULT', active: self.subscriptionLevel >= 3 }
-	];
+	self.components = [];
 
 	self.formatPricePerMonth = function (yearlyPayment) {
 		var price = isNaN(self.pricePerMonth) ? 0 : parseFloat(self.pricePerMonth);
