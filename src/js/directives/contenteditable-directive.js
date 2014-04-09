83ffd88e32c752494d5d0d81551dded3d84ee122
dach@surgeforward.com <unknown@kiln.example.com>
2014-04-10 02:27:35 +0400

    Case #26530: Make all items in personas table editable inline.

--- src/js/directives/contenteditable-directive.js
+++ src/js/directives/contenteditable-directive.js
@@ -2,9 +2,14 @@
 	return {
 		require: 'ngModel',
 		scope: {
-			onchange: '=onChange'
+			onchange: '=onChange',
+			placeholder: '=placeHolder'
 		},
-		link: function(scope, element, attrs, ctrl, ngModel) {
+		link: function (scope, element, attrs, ctrl, ngModel) {
+			if (!launch.utils.isBlank(scope.placeholder)) {
+				$(element).attr('data-placeholder', scope.placeholder);
+			}
+
 			// view -> model
 			element.on('blur', function () {
 				scope.$apply(function() {
