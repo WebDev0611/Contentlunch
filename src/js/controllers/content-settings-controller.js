233f25786242fa77f9c30169ade7e2625b8d6caa
dach@surgeforward.com <unknown@kiln.example.com>
2014-04-18 23:06:51 +0400

    Case #26530: Start hooking up the content settings to the API.

--- src/js/controllers/content-settings-controller.js
+++ src/js/controllers/content-settings-controller.js
@@ -1,9 +1,20 @@
 ﻿launch.module.controller('ContentSettingsController', [
-	'$scope', 'AuthService', 'AccountService', 'UserService', 'NotificationService', function ($scope, authService, accountService, userService, notificationService) {
+	'$scope', 'AuthService', 'AccountService', 'UserService', 'ContentSettingsService', 'NotificationService', function ($scope, authService, accountService, userService, contentSettingsService, notificationService) {
 		var self = this;
 
+		self.loggedInUser = null;
+
 		self.init = function () {
-			$scope.refreshContentSettings();
+			self.loggedInUser = authService.userInfo();
+
+			$scope.contentSettings = contentSettingsService.get(self.loggedInUser.account.id, {
+				success: function(r) {
+				},
+				error: function(r) {
+					launch.utils.handleAjaxErrorResponse(r, notificationService);
+				}
+			});
+
 			$scope.contentTypes = launch.config.CONTENT_TYPES;
 		};
 
@@ -25,11 +36,6 @@
 
 		$scope.editPersonaValue = function (value) {
 			// TODO: SAVE TO API AFTER A CHANGE TO THE PERSONA PROPERTIES!!
-		};
-
-		$scope.refreshContentSettings = function () {
-			// TODO: LOAD THIS FROM THE API ONCE IT'S IN PLACE!!
-			$scope.contentSettings = new launch.ContentSettings();
 		};
 
 		self.init();
