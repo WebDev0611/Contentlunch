4f2e5ce09e42e424b3598cdf1192654e467c33b2
dach@surgeforward.com <unknown@kiln.example.com>
2014-04-23 21:19:09 +0400

    Case #26529: Finish up tweaks to content settings UI.

--- src/js/controllers/content-settings-controller.js
+++ src/js/controllers/content-settings-controller.js
@@ -1,5 +1,5 @@
 ﻿launch.module.controller('ContentSettingsController', [
-	'$scope', 'AuthService', 'AccountService', 'UserService', 'ContentSettingsService', 'NotificationService', function ($scope, authService, accountService, userService, contentSettingsService, notificationService) {
+	'$scope', '$location', '$modal', 'AuthService', 'AccountService', 'UserService', 'ContentSettingsService', 'NotificationService', function ($scope, $location, $modal, authService, accountService, userService, contentSettingsService, notificationService) {
 		var self = this;
 
 		self.loggedInUser = null;
@@ -12,16 +12,25 @@
 
 			$scope.contentTypes = launch.config.CONTENT_TYPES;
 
-			$scope.$on('$locationChangeStart', function (e, newLocation, oldLocation) {
+			$scope.$on('$locationChangeStart', function(e, next, current) {
 				if (self.isDirty === true) {
-					self.updateContentSettings();
+					$scope.confirmCancel(false, function() {
+						$location.path(next.replace($location.protocol() + '://' + $location.host(), ''));
+					});
+
+					e.preventDefault();
 				}
 			});
 		};
 
-		self.refreshContentSettings = function() {
+		self.refreshContentSettings = function (onAfterSave) {
 			$scope.contentSettings = contentSettingsService.get(self.loggedInUser.account.id, {
-				success: function(r) {
+				success: function (r) {
+					self.isDirty = false;
+
+					if ($.isFunction(onAfterSave)) {
+						onAfterSave();
+					}
 				},
 				error: function(r) {
 					launch.utils.handleAjaxErrorResponse(r, notificationService);
@@ -29,21 +38,35 @@
 			});
 		};
 
-		self.updateContentSettings = function() {
+		$scope.contentSettings = null;
+		$scope.isSaving = false;
+		$scope.titlePlaceholder = 'Enter a Title';
+		$scope.itemPlaceholder = 'Enter Some Text';
+		$scope.textEditorSettings = launch.config.TINY_MCE_SETTINGS;
+
+		$scope.updateContentSettings = function (refresh, onAfterSave) {
+			$scope.isSaving = true;
+
 			contentSettingsService.update($scope.contentSettings, {
 				success: function (r) {
+					$scope.isSaving = false;
+					self.isDirty = false;
 					notificationService.success('Success!!', 'Successfully saved Content Settings!');
+
+					if (refresh) {
+						self.refreshContentSettings();
+					}
+
+					if ($.isFunction(onAfterSave)) {
+						onAfterSave();
+					}
 				},
 				error: function (r) {
+					$scope.isSaving = false;
 					launch.utils.handleAjaxErrorResponse(r, notificationService);
 				}
 			});
 		};
-
-		$scope.contentSettings = null;
-		$scope.titlePlaceholder = 'Enter a Title';
-		$scope.itemPlaceholder = 'Enter Some Text';
-		$scope.textEditorSettings = launch.config.TINY_MCE_SETTINGS;
 
 		$scope.setDirty = function() {
 			self.isDirty = true;
@@ -56,6 +79,32 @@
 		$scope.deletePersona = function(index) {
 			$scope.contentSettings.deletePersona(index);
 			self.isDirty = true;
+		};
+
+		$scope.confirmCancel = function (isFromUi, onAfterSave) {
+			if (self.isDirty) {
+				$modal.open({
+					templateUrl: 'confirm.html',
+					controller: [
+						'$scope', '$modalInstance', function(scp, instance) {
+							scp.message = 'You have not saved your changes. Do you you want to discard your changes?';
+							scp.okButtonText = isFromUi ? 'Cancel' : 'Save Changes';
+							scp.cancelButtonText = 'Discard Changes';
+							scp.onOk = function () {
+								if (!isFromUi) {
+									$scope.updateContentSettings(true, onAfterSave);
+								}
+
+								instance.close();
+							};
+							scp.onCancel = function() {
+								self.refreshContentSettings(onAfterSave);
+								instance.dismiss('cancel');
+							};
+						}
+					]
+				});
+			}
 		};
 
 		$scope.formatContentTypeItem = function (item, element, context) {
