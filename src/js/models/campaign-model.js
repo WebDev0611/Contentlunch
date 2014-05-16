f4c38765a30f53cca91e1d8ddcb1ef5436135dfb
dach@surgeforward.com <unknown@kiln.example.com>
2014-05-17 02:06:48 +0700

    Case #25937: More work on the create/edit content form.

---
+++ src/js/models/campaign-model.js
@@ -0,0 +1,18 @@
+﻿launch.Campaign = function() {
+	var self = this;
+
+	self.id = null;
+	self.title = null;
+	self.description = null;
+	self.goals = null;
+	self.campaignType = null;
+
+	self.startDate = null;
+	self.endDate = null;
+	self.isRecurring = false;
+	self.tags = null;
+
+	self.user = null;
+
+	return self;
+};
