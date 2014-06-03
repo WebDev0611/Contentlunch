3762898b6d4c7b82bf9e60ae39962eaf711ac1cd
dach@surgeforward.com <unknown@kiln.example.com>
2014-06-04 05:18:48 +0700

    Comment out PHP code that is causing me problems.

--- app/models/Subscription.php
+++ app/models/Subscription.php
@@ -6,9 +6,9 @@ class Subscription extends Ardent {
 
   protected $table = 'subscriptions';
 
-  protected function getDateFormat()
-  {
-    return 'Y-m-d H:i:s';
-  }
+  //protected function getDateFormat()
+  //{
+  //  return 'Y-m-d H:i:s';
+  //}
 
 }
