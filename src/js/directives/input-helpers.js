ff5f9a5721ad8bbc4e9306843b867263161b31b3
Cameron Spear <cameronspear@gmail.com>
2014-06-10 11:24:46 -0700

    Fix Daniel's issue with checkboxes.

--- src/js/directives/input-helpers.js
+++ src/js/directives/input-helpers.js
@@ -2,14 +2,12 @@ angular.module('launch')
 
 // this one helps checkboxes to use 1/0 as true/false
 // i.e. the server passes back 1 for true
-.directive('input', [function () {
+.directive('checkInt', [function () {
     return {
-        restrict: 'E',
         require: '?ngModel',
         priority: 1, // this needs to execute before Angular's
         link: function (scope, elems, attrs, ngModel) {
-            if (!ngModel) return;
-            if (attrs.type !== 'checkbox') return;
+            if (!ngModel || attrs.type !== 'checkbox' || attrs.ngTrueValue) return;
 
             // this allows any truthy value to be initally passed in
             // to the checkbox and it will still show up as checked
