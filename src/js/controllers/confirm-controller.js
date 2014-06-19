b90dfc177e07a1bf85f1d9a4a4dd6ff59b285b4a
dach@surgeforward.com <unknown@kiln.example.com>
2014-06-20 02:38:22 +0700

    Case #27661: Fix an issue with the user confirmation. Made changes to change password controller but not to confirm controller.

--- src/js/controllers/confirm-controller.js
+++ src/js/controllers/confirm-controller.js
@@ -18,45 +18,51 @@ launch.module.controller('ConfirmController', [
 		};
 
 		$scope.isLoaded = false;
-		$scope.newPassword = null;
-		$scope.confirmPassword = null;
 		$scope.isSaving = false;
 		$scope.isConfirmUser = true;
 		$scope.isDisabled = false;
 		$scope.selectedUser = null;
-		$scope.newPasswordError = null;
 		$scope.confirmPasswordError = null;
+		$scope.changePassword = {
+			currentPassword: null,
+			newPassword: null,
+			confirmPassword: null
+		};
 
 		$scope.changePassword = function(e) {
 			if (e.type === 'keypress' && e.charCode !== 13) {
 				return;
 			}
 
-			$scope.newPasswordError = launch.utils.isBlank($scope.newPassword) ? 'New Password is required.' : launch.utils.validatePassword($scope.newPassword);
-			$scope.confirmPasswordError = launch.utils.isBlank($scope.confirmPassword) ? 'Confirm Password is required.' : (($scope.newPassword !== $scope.confirmPassword) ? 'Passwords do not match.' : null);
+			var msg = '';
 
-			if (launch.utils.isBlank($scope.newPasswordError) && launch.utils.isBlank($scope.confirmPasswordError)) {
-				$scope.selectedUser.password = $scope.newPassword;
-				$scope.selectedUser.passwordConfirmation = $scope.confirmPassword;
+			msg += (launch.utils.isBlank($scope.changePassword.newPassword) ? 'New Password is required.' : launch.utils.validatePassword($scope.changePassword.newPassword)) + '\n';
+			msg += launch.utils.isBlank($scope.changePassword.confirmPassword) ? 'Confirm Password is required.\n' : (($scope.changePassword.newPassword !== $scope.changePassword.confirmPassword) ? 'Passwords do not match.\n' : '');
+
+			if (launch.utils.isBlank(msg)) {
+				$scope.selectedUser.password = $scope.changePassword.newPassword;
+				$scope.selectedUser.passwordConfirmation = $scope.changePassword.confirmPassword;
 				$scope.isSaving = true;
 
 				userService.update($scope.selectedUser, {
-					success: function(r) {
+					success: function (r) {
 						$scope.isSaving = false;
 						notificationService.success('Success!', 'You have successfully changed your password!');
 
 						authService.login($scope.selectedUser.userName, $scope.selectedUser.password, false, {
-							success: function() {
+							success: function () {
 								$location.path('/home');
 							},
-							error: function(res) { }
+							error: function (res) { }
 						});
 					},
-					error: function(r) {
+					error: function (r) {
 						$scope.isSaving = false;
 						launch.utils.handleAjaxErrorResponse(r, notificationService);
 					}
 				});
+			} else {
+				notificationService.error('Error!', '' + msg);
 			}
 		};
 
