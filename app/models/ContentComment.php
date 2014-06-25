07785222ef307136eb14711ac0f296d29e0bb843
Cameron Spear <cameronspear@gmail.com>
2014-06-25 11:00:44 -0700

    This should make guest_id OR user_id required: pass if you have one, fail if you have neither.

--- app/models/ContentComment.php
+++ app/models/ContentComment.php
@@ -11,14 +11,31 @@ class ContentComment extends Ardent {
   public $forceEntityHydrationFromInput = true;
 
   protected $fillable = [
-    'user_id', 'content_id', 'comment'
+    'user_id', 
+    'guest_id',
+    'content_id', 
+    'comment',
   ];
 
   public static $rules = [
-    'user_id' => 'required',
+    'user_id'    => 'required',
+    'guest_id'   => 'required',
     'content_id' => 'required',
-    'comment' => 'required'
+    'comment'    => 'required',
   ];
+
+  public function validate(array $rules = [], array $customMessages = []) {
+    // merge any custom rules with our standard rules
+    $rules = array_merge(self::$rules, $rules);
+    // only one or the other guest_id or user_id is required
+    if ($this->user_id) {
+      unset($rules['guest_id']);
+    }
+    if ($this->guest_id) {
+      unset($rules['user_id']);
+    }
+    return parent::validate($rules , $customMessages);
+  }
 
   public function user()
   {
