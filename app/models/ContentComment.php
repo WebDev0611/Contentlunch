eaacbddc846a85632ca97516f3c0a10eb2589e50
Cameron Spear <cameronspear@gmail.com>
2014-06-25 12:32:15 -0700

    Temporarily disable guest/user ID for comments.

--- app/models/ContentComment.php
+++ app/models/ContentComment.php
@@ -18,24 +18,24 @@ class ContentComment extends Ardent {
   ];
 
   public static $rules = [
-    'user_id'    => 'required',
-    'guest_id'   => 'required',
+    // 'user_id'    => 'required',
+    // 'guest_id'   => 'required',
     'content_id' => 'required',
     'comment'    => 'required',
   ];
 
-  public function validate(array $rules = [], array $customMessages = []) {
-    // merge any custom rules with our standard rules
-    $rules = array_merge(self::$rules, $rules);
-    // only one or the other guest_id or user_id is required
-    if ($this->user_id) {
-      unset($rules['guest_id']);
-    }
-    if ($this->guest_id) {
-      unset($rules['user_id']);
-    }
-    return parent::validate($rules , $customMessages);
-  }
+  // public function validate(array $rules = [], array $customMessages = []) {
+  //   // merge any custom rules with our standard rules
+  //   $rules = array_merge(self::$rules, $rules);
+  //   // only one or the other guest_id or user_id is required
+  //   if ($this->user_id) {
+  //     unset($rules['guest_id']);
+  //   }
+  //   if ($this->guest_id) {
+  //     unset($rules['user_id']);
+  //   }
+  //   return parent::validate($rules , $customMessages);
+  // }
 
   public function user()
   {
