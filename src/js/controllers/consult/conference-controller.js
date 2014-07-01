4f489d1176b0f1932067646c9b72efb38bb22f74
Jason Kuchynka <jkuchynka@surgeforward.com>
2014-07-01 12:23:00 -0700

    Add conference popup for account users

--- src/js/controllers/consult/conference-controller.js
+++ src/js/controllers/consult/conference-controller.js
@@ -1,5 +1,5 @@
 
-launch.module.controller('ConsultConferenceController', function ($scope, $filter, AuthService, ConferenceService, NotificationService) {
+launch.module.controller('ConsultConferenceController', function ($scope, $filter, $modal, AuthService, ConferenceService, ModelMapperService, NotificationService) {
 
   $scope.user = AuthService.userInfo();
 
@@ -39,6 +39,61 @@ launch.module.controller('ConsultConferenceController', function ($scope, $filte
   };
 
   $scope.conferences = [];
+
+  $scope.conferenceDetail = function (conference) {
+    var parentScope = $scope;
+    $modal.open({
+      windowClass: 'modal-large',
+      templateUrl: '/assets/views/consult/conference-form.html',
+      controller: function ($scope, $filter, $modalInstance, ConferenceService, NotificationService) {
+
+        $scope.mode = 'view';
+        $scope.conference = conference;
+        $scope.conference.account_id = parseInt($scope.conference.account_id);
+        
+        if ( ! $scope.conference.scheduled_date) {
+        
+        } else {
+          $scope.conference.scheduled_date_time = $scope.conference.scheduled_date;
+        }
+
+        // Topic options
+        $scope.topicOptions = [
+          ['marketing', 'Content Marketing'],
+          ['strategy', 'Content Strategy'],
+          ['application', 'Content Launch Application'],
+          ['ideation', 'Content Ideation/Writing'],
+          ['publishing', 'Content Publishing'],
+          ['promotion', 'Content Promotion']
+        ];
+
+        // Account options
+        $scope.accountOptions = [];
+
+        // Consultant options
+        // If topic is application, CEO cannot be selected
+        $scope.consultantOptions = function () {
+          if ($scope.conference.topic == 'application') {
+            return [['professional', 'Content Launch Professional']];
+          }
+          return [
+            ['professional', 'Content Launch Professional'],
+            ['ceo', 'CEO Jon Wuebben']
+          ];
+        };
+
+        $scope.users = [
+          ModelMapperService.user.fromDto($scope.conference.user)
+        ];
+
+        $scope.close = function () {
+          $modalInstance.dismiss('cancel');
+        };
+
+      }
+
+    });
+  };
 
   // Schedule date requests must be > 3 days and < 2 weeks
   $scope.minDate = moment().add('days', 4);
