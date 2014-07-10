4bf9207f62fe752ca88b04450275a225b773d50c
Cameron Spear <cameronspear@gmail.com>
2014-07-10 11:19:20 -0700

    Move Beanstalk console to /public on cleanup.

--- puphpet/files/exec-always/cleanup.sh
+++ puphpet/files/exec-always/cleanup.sh
@@ -5,3 +5,5 @@ mkdir /var/www/public/adminer/
 mv /var/www/adminer/index.php /var/www/public/adminer/index.php
 rmdir /var/www/adminer/
 rm -Rf /var/www/default/
+cp -R /var/www/beanstalk_console/ /var/www/public/beanstalk_console/
+rm -Rf /var/www/beanstalk_console/
