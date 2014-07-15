01a33e8234ad8a3effb88b46e0b5b66aa9b56690
dach@surgeforward.com <unknown@kiln.example.com>
2014-07-16 00:12:23 +0700

    Case #29917: Create a shell for promote settings at the account level. Also orgainze all the account views into folder.

---
+++ src/js/controllers/promote-settings-controller.js
@@ -0,0 +1,11 @@
+﻿launch.module.controller('PromoteSettingsController', [
+	'$scope','$location', 'AuthService', 'AccountService', 'NotificationService', function($scope, $location, authService, accountService, notificationService) {
+		var self = this;
+
+		self.init = function () {
+			
+		};
+
+		self.init();
+	}
+]);
