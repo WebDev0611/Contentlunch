92153b4b4ea8ab0000e92ed0b97d418755ec53a3
dach@surgeforward.com <unknown@kiln.example.com>
2014-07-16 03:43:31 +0700

    Case #29917: Start UI for admins to set up promote connections.

--- src/js/controllers/promote-settings-controller.js
+++ src/js/controllers/promote-settings-controller.js
@@ -6,6 +6,21 @@
 			
 		};
 
+		$scope.providerIsConnected = function(provider) {
+			// TODO: IMPLEMENT WHETHER THERE IS A CONNECTION TO THE SPECIFIED PROVIDER!!
+			return false;
+		};
+
+		$scope.connect = function (provider) {
+			// TODO: IMPLEMENT CONNECTION TO THE SPECIFIED PROVIDER!!
+			console.log('Connecting to ' + provider);
+		};
+
+		$scope.disconnect = function (provider) {
+			// TODO: IMPLEMENT DISCONNECTION FROM THE SPECIFIED PROVIDER!!
+			console.log('Disconnecting from ' + provider);
+		};
+
 		self.init();
 	}
 ]);
