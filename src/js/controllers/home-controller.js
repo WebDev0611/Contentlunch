3df17a608398f1005f15fd6bbccf070656838890
Cameron Spear <cameronspear@gmail.com>
2014-07-15 15:17:21 -0700

    Dashboard UI, including notifications/announcements. Case 29821.

--- src/js/controllers/home-controller.js
+++ src/js/controllers/home-controller.js
@@ -1,5 +1,50 @@
-launch.module.controller('HomeController', [
-	'$scope', '$location', function ($scope, $location) {
-		$scope.title = 'This is the home page controller';
-	}
+launch.module.controller('HomeController', 
+            ['$scope', '$rootScope', '$location', 'AuthService', '$q', 'NotificationService', 'Restangular', 
+    function ($scope,   $rootScope,   $location,   AuthService,   $q,   notify,                Restangular) {
+        $scope.isLoaded = false;
+
+        var user = $scope.user = AuthService.userInfo();
+        $scope.user.name = $scope.user.displayName;
+
+        // Restangular Models
+        var Account = Restangular.one('account', $scope.user.account.id);
+        var Discussion = Account.all('discussion');
+
+        $q.all({
+            discussion: Discussion.getList(),
+        }).then(function (responses) {
+            angular.extend($scope, responses);
+            $scope.isLoaded = true;
+        });
+
+
+        // Discussion
+        // -------------------------
+        $scope.createReply = function (reply) {
+            reply.userId = user.id;
+            reply.accountId = (user.account || {}).id;
+
+            Discussion.post(reply).then(function (newReply) {
+                reply.body = '';
+                notify.success('Reply created');
+                _.appendOrUpdate($scope.discussion, newReply);
+            }).catch($rootScope.globalErrorHandler);
+        };
+
+
+        // Filters
+        // -------------------------
+        $scope.filters = {
+            activity: {
+                justMine: true
+            }
+        };
+
+        // Paging
+        // -------------------------
+        $scope.pagination = {
+            pageSize: 10,
+            currentPage: 1,
+        };
+    }
 ]);
