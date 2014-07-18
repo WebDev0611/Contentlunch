5cbf624257f2f032b4d28ea27724fa7f127487b4
dach@surgeforward.com <unknown@kiln.example.com>
2014-07-18 22:49:19 +0700

    Case #30201 and 30202: Clean up code.

--- src/js/models/content-type-model.js
+++ src/js/models/content-type-model.js
@@ -7,122 +7,30 @@
 	self.baseType = null;
 
 	self.allowText = function () {
-		if (self.baseType === 'audio' || self.baseType === 'blog_post' || self.baseType === 'email' || self.baseType === 'long_html' ||
-			self.baseType === 'photo' || self.baseType === 'social_media_post' || self.baseType === 'video') {
-			return true;
-		}
-
-		//switch (self.name) {
-		//	case 'blog-post':
-		//	case 'casestudy':
-		//	case 'ebook':
-		//	case 'email':
-		//	case 'facebook-post':
-		//	case 'feature-article':
-		//	case 'google-plus-update':
-		//	case 'newsletter':
-		//	case 'landing-page':
-		//	case 'linkedin-update':
-		//	case 'photo':
-		//	case 'sales-letter':
-		//	case 'sellsheet-content':
-		//	case 'tweet':
-		//	case 'video':
-		//	case 'website-page':
-		//	case 'whitepaper':
-		//	case 'workflow-email':
-		//		return true;
-		//}
-
-		return false;
+		return (self.baseType === 'audio' || self.baseType === 'blog_post' || self.baseType === 'email' || self.baseType === 'long_html' ||
+				self.baseType === 'photo' || self.baseType === 'social_media_post' || self.baseType === 'video');
 	};
 
 	self.requireText = function () {
-		if (self.baseType === 'blog_post' || self.baseType === 'email' || self.baseType === 'long_html' || self.baseType === 'social_media_post') {
-			return true;
-		}
-
-		//switch (self.name) {
-		//	case 'blog-post':
-		//	case 'casestudy':
-		//	case 'ebook':
-		//	case 'email':
-		//	case 'feature-article':
-		//	case 'newsletter':
-		//	case 'landing-page':
-		//	case 'sales-letter':
-		//	case 'sellsheet-content':
-		//	case 'website-page':
-		//	case 'whitepaper':
-		//	case 'workflow-email':
-		//		return true;
-		//}
-
-		return false;
+		return  (self.baseType === 'blog_post' || self.baseType === 'email' || self.baseType === 'long_html' || self.baseType === 'social_media_post');
 	};
 
 	self.allowFile = function () {
-		if (self.baseType === 'attached_file' || self.baseType === 'audio' || self.baseType === 'document' || self.baseType === 'email'
-			|| self.baseType === 'photo' || self.baseType === 'social_media_post' || self.baseType === 'video') {
-			return true;
-		}
-
-		//switch (self.name) {
-		//	case 'audio-recording':
-		//	case 'email':
-		//	case 'facebook-post':
-		//	case 'google-drive-doc':
-		//	case 'google-plus-update':
-		//	case 'linkedin-update':
-		//	case 'photo':
-		//	case 'salesforce-asset':
-		//	case 'tweet':
-		//	case 'video':
-		//		return true;
-		//}
-
-		return false;
+		return (self.baseType === 'attached_file' || self.baseType === 'audio' || self.baseType === 'document' || self.baseType === 'email' ||
+				self.baseType === 'photo' || self.baseType === 'social_media_post' || self.baseType === 'video');
 	};
 
 	self.requireFile = function () {
-		if (self.baseType === 'attached_file' || self.baseType === 'audio' || self.baseType === 'document'
-			|| self.baseType === 'photo' || self.baseType === 'video') {
-			return true;
-		}
-
-		//switch (self.name) {
-		//	case 'audio-recording':
-		//	case 'google-drive-doc':
-		//	case 'photo':
-		//	case 'salesforce-asset':
-		//	case 'video':
-		//		return true;
-		//}
-
-		return false;
+		return (self.baseType === 'attached_file' || self.baseType === 'audio' || self.baseType === 'document' ||
+				self.baseType === 'photo' || self.baseType === 'video');
 	};
 
 	self.allowExport = function () {
-		if (self.baseType === 'blog_post' || self.baseType === 'email' || self.baseType === 'long_html') {
-			return true;
-		}
-
-		//switch (self.name) {
-		//	case 'blog-post':
-		//	case 'email':
-		//	case 'landing-page':
-		//		return true;
-		//}
-
-		return false;
+		return (self.baseType === 'blog_post' || self.baseType === 'email' || self.baseType === 'long_html');
 	};
 
 	self.allowMetaTags = function () {
-		if (self.baseType === 'long_html' || self.baseType === 'blog_post') {
-			return true;
-		}
-
-		return false;
+		return (self.baseType === 'long_html' || self.baseType === 'blog_post');
 	};
 
 	return self;
