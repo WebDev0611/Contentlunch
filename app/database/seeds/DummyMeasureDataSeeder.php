41a1122dc806fdcf6276e6fedc2341ffdc03c30d
Cameron Spear <cameronspear@gmail.com>
2014-07-30 11:19:05 -0700

    Dummy Measure Data Seeder.

--- app/database/seeds/DummyMeasureDataSeeder.php
+++ app/database/seeds/DummyMeasureDataSeeder.php
@@ -13,16 +13,21 @@ class DummyMeasureDataSeeder extends Seeder {
 
         // get list of users that COULD create content
         $userIDs = User::where('id', '!=', 1)->lists('id');
-        $contentTypeIDs = [2, 4, 10, 15, 5];
+        $contentIDs = Content::where('account_id', 1)->lists('id');
+
+        // Content
+        // -------------------------
+        $contentTypeIDs = [2, 4, 10, 15, 5]; // content types I KNOW don't take extra fields
 
         Content::where('title', 'LIKE', 'Dummy Measure Data %')->delete();
 
-        foreach(range(1, 50) as $index)
-        {
+        foreach (range(1, 50) as $index) {
             $userID        = $userIDs[array_rand($userIDs)];
             $contentTypeID = $contentTypeIDs[array_rand($contentTypeIDs)];
             $launched      = $faker->dateTimeThisMonth(Carbon::now()->endOfMonth());
             $created       = $faker->dateTimeThisMonth($launched);
+            $concept       = $faker->dateTimeBetween($created, $launched);
+
             Content::create([
                 'title'                  => "Dummy Measure Data {$index}",
                 'body'                   => $faker->text,
@@ -35,10 +40,10 @@ class DummyMeasureDataSeeder extends Seeder {
                 'secondary_buying_stage' => 'prospects',
                 'secondary_persona'      => 'vp sales',
                 'concept'                => $faker->text,
-                'status'                 => 1,
+                'status'                 => 4, // 4 is launched
                 'archived'               => 0,
                 'upload_id'              => null,
-                'convert_date'           => null,
+                'convert_date'           => $index < 25 ? null : $concept,
                 'submit_date'            => null,
                 'approval_date'          => null,
                 'launch_date'            => $launched,
@@ -50,6 +55,55 @@ class DummyMeasureDataSeeder extends Seeder {
                 'updated_at'             => $created,
             ]);
         }
+
+        // Tasks
+        // -------------------------
+        $contentTaskGroupIDs = ContentTaskGroup::whereIn('content_id', $contentIDs)->lists('id');
+        $campaignIDs = Campaign::where('account_id', 1)->lists('id');
+
+        ContentTask::where('name', 'LIKE', 'Dummy Measure Data %')->delete();
+        CampaignTask::where('name', 'LIKE', 'Dummy Measure Data %')->delete();
+
+        foreach (range(1, 100) as $index) {
+            $userID             = $userIDs[array_rand($userIDs)];
+            $campaignID         = $campaignIDs[array_rand($campaignIDs)];
+            $contentTaskGroupID = $contentTaskGroupIDs[array_rand($contentTaskGroupIDs)];
+            $dueDate = $faker->dateTimeThisMonth();
+            $isComplete = rand(0, 1);
+            $dateCompleted = $isComplete ? $faker->dateTimeBetween(Carbon::parse($dueDate->format('Y-m-d H:i:s'))->subWeek(1)->format('Y-m-d H:i:s'), $dueDate) : null;
+
+            $data = [
+                'name' => "Dummy Measure Data {$index}",
+                'due_date' => $dueDate,
+                'date_completed' => $dateCompleted,
+                'is_complete' => $isComplete,
+                'user_id' => $userID,
+                'content_task_group_id' => $contentTaskGroupID,
+            ];
+
+            ContentTask::create($data);
+
+            unset($data['content_task_group_id']);
+            $data['campaign_id'] = $campaignID;
+
+            CampaignTask::create($data);
+        }
+
+
+        // Seed Measure stuff
+        // -------------------------
+        $date = Carbon::now()->subMonth(1);
+        $now  = Carbon::now();
+
+        do {
+            App::make('MeasureController')->measureCreatedContent($date->format('Y-m-d'), 1);
+            App::make('MeasureController')->measureLaunchedContent($date->format('Y-m-d'), 1);
+            App::make('MeasureController')->measureTimingContent($date->format('Y-m-d'), 1);
+
+            $date->addDay(1);
+        } while ($now->gte($date));
+
+        App::make('MeasureController')->measureUserEfficiency(1);
     }
 
 }
