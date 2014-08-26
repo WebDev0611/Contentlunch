84ff81023bd5033007d2c59a3d3351a533e392b1
Chris <yeung.chris@gmail.com>
2014-08-26 10:43:53 -0400

    Fix account_connection_add_external_id migration

--- app/database/migrations/2014_08_22_000332_account_connection_add_external_id.php
+++ app/database/migrations/2014_08_22_000332_account_connection_add_external_id.php
@@ -22,24 +22,26 @@ class AccountConnectionAddExternalId extends Migration {
 		$connections = AccountConnection::with('connection')->get();
 		$logs = [];
 		foreach ($connections as $connection) {
-			$api = ConnectionConnector::loadAPI($connection->connection->provider, $connection);
-			if ( ! $connection->external_id) {
-				try {
-					$log = '';
-					$log .= 'Updating connection external_id, id: '. $connection->id .' provider: '. $connection->connection->provider .' result: ';
-					$externalId = $api->getExternalId();
-					$connection->external_id = $externalId;
-					$connection->updateUniques();
-					if ($externalId) {
-						$log .= 'success: '. $externalId;
-					} else {
-						$log .= 'failed, no id found';
-					}
-				} catch (\Exception $e) {
-					$log .= 'failed, exception thrown: '. $e->getMessage();
-				}
-				$logs[] = $log;
-			}
+            if(ConnectionConnector::existsAPI($connection->connection->provider)) {
+                $api = ConnectionConnector::loadAPI($connection->connection->provider, $connection);
+                if ( ! $connection->external_id) {
+                    try {
+                        $log = '';
+                        $log .= 'Updating connection external_id, id: '. $connection->id .' provider: '. $connection->connection->provider .' result: ';
+                        $externalId = $api->getExternalId();
+                        $connection->external_id = $externalId;
+                        $connection->updateUniques();
+                        if ($externalId) {
+                            $log .= 'success: '. $externalId;
+                        } else {
+                            $log .= 'failed, no id found';
+                        }
+                    } catch (\Exception $e) {
+                        $log .= 'failed, exception thrown: '. $e->getMessage();
+                    }
+                    $logs[] = $log;
+                }
+            }
 		}
 	}
 
