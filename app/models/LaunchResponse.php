cafc21089af92f836aa2dacb78a93552eb84e25e
Jason Kuchynka <jkuchynka@surgeforward.com>
2014-09-05 14:08:00 -0700

    Fix exception when getting launch response url

--- app/models/LaunchResponse.php
+++ app/models/LaunchResponse.php
@@ -37,23 +37,30 @@ class LaunchResponse extends Ardent {
     if (empty($this->account_connection->connection_id)) {
       return;
     }
-    $connection = Connection::withTrashed()->find($this->account_connection->connection_id);
-    $response = unserialize($this->response);
-    switch ($connection->provider) {
-      case 'blogger':
-        return $response->getUrl();
-      case 'soundcloud':
-        return $response['permalink_url'];
-      case 'tumblr':
-        $api = ConnectionConnector::loadAPI('tumblr', $this->account_connection);
-        $info = $api->getMe();
-        return $info['user']['blogs'][0]['url'] . $response['response']['id'];
-      case 'vimeo':
-        return 'https://vimeo.com/' . $response['video_id'];
-      case 'wordpress':
-        return $response['URL'];
-      case 'youtube':
-        return 'https://www.youtube.com/watch?v=' . $response->getId();
+    try {
+      $connection = Connection::withTrashed()->find($this->account_connection->connection_id);
+      $response = unserialize($this->response);
+      switch ($connection->provider) {
+        case 'blogger':
+          return $response->getUrl();
+        case 'soundcloud':
+          if ( ! empty($response['permalink_url'])) {
+            return $response['permalink_url'];
+          }
+          break;
+        case 'tumblr':
+          $api = ConnectionConnector::loadAPI('tumblr', $this->account_connection);
+          $info = $api->getMe();
+          return $info['user']['blogs'][0]['url'] . $response['response']['id'];
+        case 'vimeo':
+          return 'https://vimeo.com/' . $response['video_id'];
+        case 'wordpress':
+          return $response['URL'];
+        case 'youtube':
+          return 'https://www.youtube.com/watch?v=' . $response->getId();
+      }
+    } catch (\Exception $e) {
+      return '';
     }
     
   }
