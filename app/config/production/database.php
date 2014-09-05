8e423b2d0a54cb0621b2e387339bfdca61907014
Jason Kuchynka <jkuchynka@surgeforward.com>
2014-09-05 14:41:33 -0700

    Add production configs

---
+++ app/config/production/database.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+  'default' => 'mysql',
+  'connections' => array(
+    'mysql' => array(
+      'driver'    => 'mysql',
+      'host'      => 'app.contentlaunch.sql01.prd.us-east-1.lan.surgeforward.com',
+      'database'  => 'contentlaunch_production',
+      'username'  => 'contentlaunch',
+      'password'  => 'Fn7E2tcA7fDPyYmY',
+      'charset'   => 'utf8',
+      'collation' => 'utf8_unicode_ci',
+      'prefix'    => '',
+    ),
+  )
+);
