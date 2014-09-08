c00e846074b7759a4d17e79f0fb5f8f79e5aefba
Jason Kuchynka <jkuchynka@surgeforward.com>
2014-09-07 22:28:33 -0700

    Don't set timezone unless one is found, fixes invalid timezone id exception

--- app/libraries/Launch/Scheduler/Timezone.php
+++ app/libraries/Launch/Scheduler/Timezone.php
@@ -12,8 +12,6 @@ class Timezone
      */
     public static function set($timezone)
     {
-        // set MySQL timezone
-        DB::statement("SET time_zone='{$timezone}'");
 
         // Convert -08:00 to "Americas/Los_Angeles" etc.
         // Note that it may not be reliable to store the timezone ID because
@@ -23,10 +21,15 @@ class Timezone
         $timezoneName = timezone_name_from_abbr(null, $offset * 3600, true);
         if ($timezoneName === false) $tz = timezone_name_from_abbr(null, $offset * 3600, false);
 
-        // set Laravel timezone
-        Config::set('app.timezone', $timezoneName);
-        // set PHP timezone
-        date_default_timezone_set($timezoneName);
+        if ($timezoneName) {
+            // set MySQL timezone
+            DB::statement("SET time_zone='{$timezone}'");
+
+            // set Laravel timezone
+            Config::set('app.timezone', $timezoneName);
+            // set PHP timezone
+            date_default_timezone_set($timezoneName);
+        }
     }
 }
 
