e3aab1990a35d6b70123b9bbc6d12b207b21fef0
dach@surgeforward.com <unknown@kiln.example.com>
2014-09-13 01:05:34 +0700

    Case #31682: Hook up button to API route to allow accounts to be renewed.

--- src/js/directives/account-form-directive.js
+++ src/js/directives/account-form-directive.js
@@ -221,20 +221,37 @@
 			e.stopImmediatePropagation();
 		};
 
-		scope.renewAccount = function() {
-			scope.isSaving = true;
+		scope.renewAccount = function () {
+			$modal.open({
+				templateUrl: 'confirm.html',
+				controller: [
+					'$scope', '$modalInstance', function (scp, instance) {
+						scp.message = 'You will be charged $' + scope.selectedAccount.subscription.formatPricePerMonth(scope.selectedAccount.yearlyPayment) + ' if you renew. Are you sure you want to renew?';
+						scp.okButtonText = 'Renew Account';
+						scp.cancelButtonText = 'Cancel';
+						scp.onOk = function () {
+							scope.isSaving = true;
 
-			AccountService.renew(scope.selectedAccount.id, {
-				success: function(r) {
-					scope.isSaving = false;
+							AccountService.renew(scope.selectedAccount.id, {
+								success: function (r) {
+									scope.isSaving = false;
 
-					NotificationService.success('SUCCESS!!', 'Your account has been charged $' + r.subscription.formatPricePerMonth(r.yearlyPayment) + '.');
-				},
-				error: function (r) {
-					scope.isSaving = false;
+									NotificationService.success('SUCCESS!!', 'Your account has been charged $' + r.subscription.formatPricePerMonth(r.yearlyPayment) + '.');
+								},
+								error: function (r) {
+									scope.isSaving = false;
 
-					launch.utils.handleAjaxErrorResponse(r, NotificationService);
-				}
+									launch.utils.handleAjaxErrorResponse(r, NotificationService);
+								}
+							});
+
+							instance.close();
+						};
+						scp.onCancel = function () {
+							instance.dismiss('cancel');
+						};
+					}
+				]
 			});
 		};
 
