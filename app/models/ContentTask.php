f7582f62456b2c443a816be427eba0147f5c3a92
Chris <yeung.chris@gmail.com>
2014-09-22 13:13:09 -0400

    Case #32125: Fix activity created when task is created

--- app/models/ContentTask.php
+++ app/models/ContentTask.php
@@ -1,69 +1,67 @@
 <?php
 
-use LaravelBook\Ardent\Ardent;
+    use LaravelBook\Ardent\Ardent;
 
-class ContentTask extends Ardent {
-
-    protected $softDelete = true;
-
-    public $autoHydrateEntityFromInput = false;
-    public $forceEntityHydrationFromInput = false;
-
-    protected $guarded = [
-        'id',
-        'task_group_id',
-        'created_at',
-        'updated_at',
-    ];
-    public static $rules = [
-        'due_date'              => 'required',
-        'name'                  => 'required',
-        'user_id'               => 'required',
-        'content_task_group_id' => 'required',
-    ];
-
-    public function content()
+    class ContentTask extends Ardent
     {
-        return $this->belongsTo('Content');
-    }
 
-    public function task_group()
-    {
-        return $this->belongsTo('ContentTaskGroup', 'content_task_group_id');
-    }
+        protected $softDelete = true;
 
-    public function user()
-    {
-        return $this->belongsTo('User');
-    }
+        public $autoHydrateEntityFromInput = false;
+        public $forceEntityHydrationFromInput = false;
 
-    public static function boot()
-    {
-      parent::boot();
+        protected $guarded = [
+            'id',
+            'task_group_id',
+            'created_at',
+            'updated_at',
+        ];
+        public static $rules = [
+            'due_date' => 'required',
+            'name' => 'required',
+            'user_id' => 'required',
+            'content_task_group_id' => 'required',
+        ];
 
-      static::created(function ($task) {
-        // Store an activity log for the content
-        $user = Confide::user();
-        $assignedUser = User::find($task->user_id);
-        // this was breaking seeding?
-        if (!$task->content) return true;
-        $contentID = $task->content->content_id;
-        $activity = new ContentActivity([
-          'user_id' => $user->id,
-          'content_id' => $contentID,
-          'activity' => "Assigned 1 task to ". strtoupper($assignedUser->first_name .' '. $assignedUser->last_name)
-        ]);
-        $activity->save();
-      });
-    }
+        public function content() {
+            return $this->belongsTo('Content');
+        }
 
-    public function afterCreate()
-    {
-        // save general activity
-        Activity::create([
-            'content_id' => $this->id,
-            'user_id'    => $this->user_id,
-            'activity'   => 'assigned a task in',
-        ]);
+        public function task_group() {
+            return $this->belongsTo('ContentTaskGroup', 'content_task_group_id');
+        }
+
+        public function user() {
+            return $this->belongsTo('User');
+        }
+
+        public static function boot() {
+            parent::boot();
+
+            static::created(function ($task) {
+                // Store an activity log for the content
+                $user = Confide::user();
+                $assignedUser = User::find($task->user_id);
+
+                $task->task_group = $task->task_group()->first();
+
+                // this was breaking seeding?
+                if (!$task->task_group->content_id) return true;
+                $contentID = $task->task_group->content_id;
+                $activity = new ContentActivity([
+                    'user_id' => $user->id,
+                    'content_id' => $contentID,
+                    'activity' => "Assigned 1 task to " . strtoupper($assignedUser->first_name . ' ' . $assignedUser->last_name)
+                ]);
+                $activity->save();
+
+                // save general activity
+                Activity::create([
+                    'content_id' => $contentID,
+                    'user_id' => $assignedUser->id,
+                    'activity' => 'assigned a task in',
+                ]);
+            });
+        }
+
     }
-}
