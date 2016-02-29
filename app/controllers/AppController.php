d407d5f597a2b02eec5fe406ec3b153d6304a348
Marc Hughes <marc.hughes@gmail.com>
2016-02-29 15:29:12 -0500

    Fix account discovery.

--- app/controllers/AppController.php
+++ app/controllers/AppController.php
@@ -19,14 +19,18 @@ class AppController extends BaseController
     function home() {
         $user = Auth::user();
         $accounts = AccountUser::where('user_id', $user->id)->get();
-        foreach($accounts as $account) {
-            if($account != 'client') {
+
+        foreach($accounts as $accountUser) {
+            $account = Account::find($accountUser->account_id);
+            if($account->account_type != 'client') {
                 // Prefer agency or pro accounts over clients.
                 return Redirect::to("/account/{$account->id}");
             }
         }
+
+        # If no agency account found, any other is fine.
         foreach($accounts as $account) {
-            return Redirect::to("/account/{$account->id}");
+            return Redirect::to("/account/{$account->account_id}");
         }
     }
 
