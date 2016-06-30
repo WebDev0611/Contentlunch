b16d59f333b428a6b78d0e824c1c3a8da638a410
Sean Lars Ganser <sean.ganser@gmail.com>
2016-06-30 19:31:21 -0400

    added the task icon action to the daily calendar view

--- src/js/2016/calendar/calendar.js
+++ src/js/2016/calendar/calendar.js
@@ -4,37 +4,38 @@
 
 (function(window,document,$){
 
-	var activeCell = null;
-	var appendTaskButton = function(cell){
-		var taskButton = $('#calendar-dropdown-template').html();
-		$(cell).append(taskButton);
-	};
-	var removeTaskButton = function(cell){
-		$(cell).find('.calendar-schedule-dropdown-wrapper').fadeOut(200,function(){
-			$(this).remove();
-		});
-	};
-    
-    var activateMonthly = function(){
-    	var selCellVal = $(this).data('cell-date');
-    	var selCell = $(this);
-    	if( $(activeCell).data('cell-date') !== selCellVal ){
-    		appendTaskButton(selCell);
-    		removeTaskButton(activeCell);
-    		activeCell = selCell;
-    	}
-   	};
-    var activateWeekly = function(){
-        var selCellVal = $(this).data('cell-date-time');
+    var activeCell = null;
+    var appendTaskButton = function(cell){
+        var taskButton = $('#calendar-dropdown-template').html();
+        $(cell).append(taskButton);
+    };
+    var removeTaskButton = function(cell){
+        $(cell).find('.calendar-schedule-dropdown-wrapper').fadeOut(200,function(){
+            $(this).remove();
+        });
+    };
+
+    var showCalActionOverlay = function(identifier){
+        var selCellVal = $(this).data(identifier);
         var selCell = $(this);
-        if( $(activeCell).data('cell-date-time') !== selCellVal ){
+        if( $(activeCell).data(identifier) !== selCellVal && !$(this).attr('disabled') ){
             appendTaskButton(selCell);
             removeTaskButton(activeCell);
             activeCell = selCell;
         }
-    };  
-   	
-    $('.calendar-month-days td').mouseover(activateMontly);
+    };
+    var activateMonthly = function(){
+        showCalActionOverlay.call(this,'cell-date');
+    };
+    var activateWeekly = function(){
+        showCalActionOverlay.call(this,'cell-date-time');
+    };      
+    var activateDaily = function(){
+        showCalActionOverlay.call(this,'cell-time');
+    };
+
+    $('.calendar-month-days td').mouseover(activateMonthly);
     $('.calendar-week-hours td').mouseover(activateWeekly);
+    $('.calendar-day td').mouseover(activateDaily);
 
 })(window,document,jQuery); 
