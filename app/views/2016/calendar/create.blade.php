d16aeec9639015aa3309294a581ee5aea8d76fb7
Sean Lars Ganser <sean.ganser@gmail.com>
2016-06-30 17:34:09 -0400

    plugged in modals and created small menu for common calendar items

---
+++ app/views/2016/calendar/create.blade.php
@@ -0,0 +1,131 @@
+<div id="createCalendarModal" class="sidemodal">
+    <div class="sidemodal-header">
+        <h4 class="sidemodal-header-title">Create new calendar</h4>
+        <button class="sidemodal-close" data-dismiss="modal">
+            <i class="icon-remove"></i>
+        </button>
+    </div>
+    <div class="sidemodal-container">
+        <div class="form-group">
+            <label for="#">Enter Calendar Name</label>
+            <input type="text" class="input" placeholder="Enter Calendar Name">
+        </div>
+        <div class="form-group">
+            <label for="#">VISIBLE CONTENT TYPES</label>
+            <ul class="sidemodal-list-items">
+                <li>
+                    <label for="BlogPost" class="checkbox-primary">
+                        <input id="BlogPost" type="checkbox">
+                        <span>Blog posts</span>
+                    </label>
+                </li>
+                <li>
+                    <label for="Article" class="checkbox-primary">
+                        <input id="Article" type="checkbox">
+                        <span>Article</span>
+                    </label>
+                </li>
+                <li>
+                    <label for="FacebookPost" class="checkbox-primary">
+                        <input id="FacebookPost" type="checkbox">
+                        <span>FacebookPost</span>
+                    </label>
+                </li>
+                <li>
+                    <label for="BlogPost" class="checkbox-primary">
+                        <input id="BlogPost" type="checkbox">
+                        <span>Blog posts</span>
+                    </label>
+                </li>
+                <li>
+                    <label for="Article" class="checkbox-primary">
+                        <input id="Article" type="checkbox">
+                        <span>Article</span>
+                    </label>
+                </li>
+                <li>
+                    <label for="FacebookPost" class="checkbox-primary">
+                        <input id="FacebookPost" type="checkbox">
+                        <span>FacebookPost</span>
+                    </label>
+                </li>
+                <li>
+                    <label for="BlogPost" class="checkbox-primary">
+                        <input id="BlogPost" type="checkbox">
+                        <span>Blog posts</span>
+                    </label>
+                </li>
+                <li>
+                    <label for="Article" class="checkbox-primary">
+                        <input id="Article" type="checkbox">
+                        <span>Article</span>
+                    </label>
+                </li>
+                <li>
+                    <label for="FacebookPost" class="checkbox-primary">
+                        <input id="FacebookPost" type="checkbox">
+                        <span>FacebookPost</span>
+                    </label>
+                </li>
+            </ul>
+        </div>
+        <div class="form-group">
+            <label for="#">Calendar Colors</label>
+            <label for="CalendarColor" class="checkbox-color CL-f3ee8b">
+                <input id="CalendarColor" type="checkbox">
+                <span></span>
+            </label>
+            <label for="CalendarColor1" class="checkbox-color CL-fdcc5b">
+                <input id="CalendarColor1" type="checkbox">
+                <span></span>
+            </label>
+            <label for="CalendarColor2" class="checkbox-color CL-edab7e">
+                <input id="CalendarColor2" type="checkbox">
+                <span></span>
+            </label>
+            <label for="CalendarColor3" class="checkbox-color CL-d68ae6">
+                <input id="CalendarColor3" type="checkbox">
+                <span></span>
+            </label>
+            <label for="CalendarColor4" class="checkbox-color CL-a089e7">
+                <input id="CalendarColor4" type="checkbox">
+                <span></span>
+            </label>
+            <label for="CalendarColor5" class="checkbox-color CL-5496cb">
+                <input id="CalendarColor5" type="checkbox">
+                <span></span>
+            </label>
+            <label for="CalendarColor6" class="checkbox-color CL-7cd1d1">
+                <input id="CalendarColor6" type="checkbox">
+                <span></span>
+            </label>
+        </div>
+        <fieldset class="form-fieldset">
+            <legend class="form-legend">Invite Guests</legend>
+            <div class="form-group">
+                <input type="text" class="input input-secondary" placeholder="Enter one of more addresses">
+            </div>
+            <div class="form-group">
+                <button class="button button-tertiary button-extend text-uppercase">Submit</button>
+            </div>
+            <label for="#">Allow Access To</label>
+            <label for="AllowAccess" class="checkbox-primary">
+                <input id="AllowAccess" type="checkbox">
+                <span>Ideas Only</span>
+            </label>
+        </fieldset>
+    </div>
+    <div class="sidemodal-footer">
+        <div class="form-group">
+            <button class="button button-extend text-uppercase">
+                Create Calendar
+            </button>
+        </div>
+    </div>
+</div>
+
+<script type="text/javascript">
+(function(){
+
+})(window,document,jQuery);
+</script>
--- app/views/2016/calendar/index.blade.php
+++ app/views/2016/calendar/index.blade.php
@@ -43,53 +43,7 @@
                     </li>
                 </ul>
             </div>
-            <div class="calender-menu-buttons-group">
-                <button class="calendar-menu-buttons">
-                    <i class="icon-calendar-add"></i>
-                </button>
-                <button class="calendar-menu-buttons">
-                    <i class="icon-calendar-view"></i>
-                </button>
-                <button class="calendar-menu-buttons">
-                    <i class="icon-cone"></i>
-                </button>
-                <div class="calendar-menu-dropdown">
-                    <button class="calendar-menu-buttons" data-toggle="dropdown">
-                        <i class="icon-reverse-direction"></i>
-                        <span class="caret"></span>
-                    </button>
-                    <ul class="dropdown-menu dropdown-menu-right">
-                        <li class="dropdown-header">Import</li>
-                        <li>
-                            <a href="#">From iCal</a>
-                        </li>
-                        <li>
-                            <a href="#">From Google Calendar</a>
-                        </li>
-                        <li class="dropdown-header">Export</li>
-                        <li>
-                            <a href="#">To Google Calendar</a>
-                        </li>
-                        <li>
-                            <a href="#">To iCal</a>
-                        </li>
-                        <li>
-                            <a href="#">To PDF</a>
-                        </li>
-                        <li>
-                            <a href="#">To Excel</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <div class="calendar-menu-switch">
-                <button type="button" class="button button-secondary button-small" onclick="window.location='/2016/daily/<?= date('Y/m/d');?>';">Today</button>
-                <div class="btn-group">
-                    <button type="button" class="button button-switches button-small active">Month</button>
-                    <button type="button" class="button button-switches button-small" onclick="window.location='/2016/weekly';">Week</button>
-                    <button type="button" class="button button-switches button-small" onclick="window.location='/2016/daily';">Day</button>
-                </div>
-            </div>
+            @include('2016.calendar.menu')
         </div>
 
        {{$calendar}}
@@ -560,6 +514,9 @@
     </div>
 </div>
 
+@include('2016.calendar.filter')
+@include('2016.calendar.task')
+@include('2016.calendar.create')
 
 
 @stop
