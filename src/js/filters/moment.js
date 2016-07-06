eb77d14da9dc890ba64d53636857bf442a02c204
Micah Potter <micah.j.potter@lowes.com>
2016-07-06 19:57:41 -0400

    removing src/js/filters/lodash.js

--- src/js/filters/moment.js
+++
@@ -1,9 +0,0 @@
-angular.module('launch')
-
-// input:   date | moment:'MM/DD/YYYY'
-// returns: the date formatted with Moment
-.filter('moment', [function () {
-    return function (date, format) {
-        return moment(date).format(format);
-    }; 
-}]);
