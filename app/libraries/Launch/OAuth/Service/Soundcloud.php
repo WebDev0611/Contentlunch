79de19b73f1d92484392fbd0d00d6da2a764cad1
Micah Potter <micah.j.potter@lowes.com>
2016-07-06 19:57:50 -0400

    removing app/libraries/Launch/OAuth/Service/AbstractServiceOAuth2.php

--- app/libraries/Launch/OAuth/Service/Soundcloud.php
+++
@@ -1,9 +0,0 @@
-<?php namespace Launch\OAuth\Service;
-
-use OAuth\OAuth2\Service\SoundCloud as OAuthSoundCloud;
-
-class Soundcloud extends OAuthSoundCloud {
-  
-  const API_NON_EXPIRE = 'non-expiring';
-
-}
