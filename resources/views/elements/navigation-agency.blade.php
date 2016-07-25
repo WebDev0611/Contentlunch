0a8afd5750286c0c833165944740736bd4171966
Sean Lars Ganser <sean.ganser@gmail.com>
2016-07-25 15:46:16 -0700

    fixed nav active links (uri segment change to 1)

--- resources/views/elements/navigation-agency.blade.php
+++ resources/views/elements/navigation-agency.blade.php
@@ -1,10 +1,10 @@
 <nav class="navigation">
     <a href="#" class="navigation-logo">
         <img src="/images/logo.svg" alt="Content Launch">
-    </a>
+    </a>{{ Request::segment(1)}}
     <ul class="navigation-menu">
         <li>
-            <a href="#" class="navigation-menu-profile">
+            <a href="javascript:;" class="navigation-menu-profile">
                 <img src="images/avatar.jpg" alt="#">
                 <div class="app-type">
                   <p class="app-agency">Agency</p>
@@ -12,49 +12,49 @@
             </a>
         </li>
         <li>
-            <a href="/#/components" class="navigation-menu-link active">
+            <a href="/agency" class="navigation-menu-link {{ ( Request::segment(1) == 'agency' ) ? 'active': ''  }}">
                 <i class="navigation-menu-icon icon-navigation-agency">
                     <span>Agency</span>
                 </i>
             </a>
         </li>
         <li>
-            <a href="/home"  class="navigation-menu-link">
+            <a href="/home" class="navigation-menu-link {{ ( Request::segment(1) == 'home' ) ? 'active': ''  }}">
                 <i class="navigation-menu-icon icon-navigation-chart">
-                    <span>Graph</span>
+                    <span>Dashboard</span>
                 </i>
             </a>
         </li>
         <li>
-            <a href="/plan" class="navigation-menu-link">
+            <a href="/plan" class="navigation-menu-link {{ ( Request::segment(1) == 'plan' ) ? 'active': ''  }}">
                 <i class="navigation-menu-icon icon-navigation-content">
-                    <span>Plan</span>
+                    <span>PLAN</span>
                 </i>
             </a>
         </li>
         <li>
-            <a href="/content" class="navigation-menu-link">
+            <a href="/content" class="navigation-menu-link {{ ( Request::segment(1) == 'content' ) ? 'active': ''  }}">
                 <i class="navigation-menu-icon icon-navigation-dashboard">
-                    <span>Create</span>
+                    <span>CREATE</span>
                 </i>
             </a>
         </li>
         <li>
-            <a href="/calendar" class="navigation-menu-link">
+            <a href="/calendar" class="navigation-menu-link {{ ( Request::segment(1) == 'calendar' ) ? 'active': ''  }}">
                 <i class="navigation-menu-icon icon-navigation-calendar">
-                    <span>Calendar</span>
+                    <span>CALENDAR</span>
                 </i>
             </a>
         </li>
         <li>
-            <a href="/collaborate" class="navigation-menu-link">
+            <a href="/collaborate" class="navigation-menu-link {{ ( Request::segment(1) == 'collaborate' ) ? 'active': ''  }}">
                 <i class="navigation-menu-icon icon-navigation-users">
                     <span>Collaborate</span>
                 </i>
             </a>
         </li>
     </ul>
-    <a href="#" class="navigation-settings">
+    <a href="/settings" class="navigation-settings {{ ( Request::segment(1) == 'settings' ) ? 'active': ''  }}">
         <i class="navigation-menu-icon icon-cog"></i>
     </a>
 </nav>
--- resources/views/elements/navigation.blade.php
+++ resources/views/elements/navigation.blade.php
@@ -9,35 +9,35 @@
             </a>
         </li>
         <li>
-            <a href="/home" class="navigation-menu-link {{ ( Request::segment(2) == 'home' ) ? 'active': ''  }}">
+            <a href="/home" class="navigation-menu-link {{ ( Request::segment(1) == 'home' ) ? 'active': ''  }}">
                 <i class="navigation-menu-icon icon-navigation-chart">
                     <span>Dashboard</span>
                 </i>
             </a>
         </li>
         <li>
-            <a href="/plan" class="navigation-menu-link {{ ( Request::segment(2) == 'plan' ) ? 'active': ''  }}">
+            <a href="/plan" class="navigation-menu-link {{ ( Request::segment(1) == 'plan' ) ? 'active': ''  }}">
                 <i class="navigation-menu-icon icon-navigation-content">
                     <span>PLAN</span>
                 </i>
             </a>
         </li>
         <li>
-            <a href="/content" class="navigation-menu-link {{ ( Request::segment(2) == 'content' ) ? 'active': ''  }}">
+            <a href="/content" class="navigation-menu-link {{ ( Request::segment(1) == 'content' ) ? 'active': ''  }}">
                 <i class="navigation-menu-icon icon-navigation-dashboard">
                     <span>CREATE</span>
                 </i>
             </a>
         </li>
         <li>
-            <a href="/calendar" class="navigation-menu-link {{ ( Request::segment(2) == 'calendar' ) ? 'active': ''  }}">
+            <a href="/calendar" class="navigation-menu-link {{ ( Request::segment(1) == 'calendar' ) ? 'active': ''  }}">
                 <i class="navigation-menu-icon icon-navigation-calendar">
                     <span>CALENDAR</span>
                 </i>
             </a>
         </li>
         <li>
-            <a href="/collaborate" class="navigation-menu-link {{ ( Request::segment(2) == 'collaborate' ) ? 'active': ''  }}">
+            <a href="/collaborate" class="navigation-menu-link {{ ( Request::segment(1) == 'collaborate' ) ? 'active': ''  }}">
                 <i class="navigation-menu-icon icon-navigation-users">
                     <span>Collaborate</span>
                 </i>
