ec17c5958502b0817b53bfdad7db29787d8655e2
Sean Lars Ganser <sean.ganser@gmail.com>
2016-08-25 14:41:28 -0700

    added backend support for Ideas, with migrations and new controller

---
+++ database/migrations/2016_08_25_212947_create_idea_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateIdeaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('idea', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('text');
+            $table->string('tags');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('idea');
+    }
+}
---
+++ database/migrations/2016_08_25_213238_create_idea_content_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateIdeaContentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('idea_content', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('idea_id')->unsigned();
+            $table->string('author');
+            $table->mediumText('body');
+            $table->string('fb_shares');
+            $table->string('google_shares');
+            $table->string('image');
+            $table->mediumText('link');
+            $table->mediumText('source');
+            $table->mediumText('title');
+            $table->string('total_shares');
+            $table->string('tw_shares');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('idea_content');
+    }
+}
