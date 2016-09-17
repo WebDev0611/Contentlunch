372f9ce7d08cf7c490ccc3fc1d1a5ce436703820
Sean Lars Ganser <sean.ganser@gmail.com>
2016-09-17 14:41:00 -0700

    unhiding the submit option - this will move it to ready_published state

--- resources/views/content/editor.blade.php
+++ resources/views/content/editor.blade.php
@@ -29,7 +29,7 @@
                                     @if (isset($content))
                                         <a href="{{ route('contentPublish', $content->id) }}" class="button button-small  ">PUBLISH</a>
                                     @endif
-                                    <div class="btn-group hidden">
+                                    <div class="btn-group">
                                         <button type="button" class="button button-small">SUBMIT</button>
                                         <button type="button" class="button button-small dropdown-toggle"
                                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
