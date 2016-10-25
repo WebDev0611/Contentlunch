406afed29e651561e260883cc64e4045feb8553c
Andre Castelo <andrecastelo@gmail.com>
2016-10-25 11:31:29 -0200

    formatting

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -3,22 +3,20 @@
 namespace App\Http\Controllers;
 
 use Auth;
-use Crypt;
 use View;
 use Session;
-use Carbon\Carbon;
-
 use App\Http\Requests\Connection\ConnectionRequest;
 use App\Http\Requests\AccountSettings\AccountSettingsRequest;
 use App\Connection;
 use App\Provider;
 use App\Helpers;
 
-class SettingsController extends Controller {
-
+class SettingsController extends Controller
+{
     public function index()
     {
         $user = Auth::user();
+
         return View::make('settings.index', compact('user'));
     }
 
@@ -39,61 +37,67 @@ class SettingsController extends Controller {
         $user->save();
 
         return redirect()->route('settingsIndex')->with([
-            'flash_message' => "Account settings updated.",
+            'flash_message' => 'Account settings updated.',
             'flash_message_type' => 'success',
-            'flash_message_important' => true
+            'flash_message_important' => true,
         ]);
     }
 
-	public function content()
+    public function content()
     {
-		return View::make('settings.content');
-	}
+        return View::make('settings.content');
+    }
 
     public function connections()
     {
-		// Pulling Connection information
-		$connections = Auth::user()->connections()->get();
-		$activeConnectionsCount = Auth::user()->connections()->where('successful',1)->count();
+        // Pulling Connection information
+        $connections = Auth::user()->connections()->get();
+        $activeConnectionsCount = Auth::user()->connections()->where('successful', 1)->count();
 
-		// - Create Connection Drop Down Data
-		$connectiondd = ['' => '-- Select One --'];
-		$connectiondd += Provider::select('slug','name')->where('class_name', '!=', '')->orderBy('name', 'asc')->distinct()->lists('name', 'slug')->toArray();
+        // - Create Connection Drop Down Data
+        $connectiondd = ['' => '-- Select One --'];
+        $connectiondd += Provider::select('slug', 'name')
+            ->where('class_name', '!=', '')
+            ->orderBy('name', 'asc')
+            ->distinct()
+            ->lists('name', 'slug')
+            ->toArray();
 
-		return View::make('settings.connections', compact('connectiondd', 'connections', 'activeConnectionsCount'));
-	}
+        return View::make('settings.connections', compact('connectiondd', 'connections', 'activeConnectionsCount'));
+    }
 
-	public function connectionCreate(ConnectionRequest $request)
+    public function connectionCreate(ConnectionRequest $request)
     {
-		$connName = $request->input('con_name');
-		$connType = $request->input('con_type');
-		$connActive = $request->input('con_active');
+        $connName = $request->input('con_name');
+        $connType = $request->input('con_type');
+        $connActive = $request->input('con_active');
 
         // - Store the conection data
-        $conn = new Connection;
+        $conn = new Connection();
         $conn->name = $connName;
         $conn->provider_id = Provider::findBySlug($connType)->id;
         $conn->active = $connActive == 'on' ? 1 : 0;
-		$conn->save();
+        $conn->save();
 
         // - Attach to the user
         Auth::user()->connections()->save($conn);
 
         Session::put('connection_data', [
             'meta_data' => $request->input('api'),
-            'connection_id' => $conn->id
+            'connection_id' => $conn->id,
         ]);
 
-		// - Lets get out of here
-		return redirect()->route('connectionProvider', $connType);
-	}
+        // - Lets get out of here
+        return redirect()->route('connectionProvider', $connType);
+    }
 
-	public function seo() {
-		return View::make('settings.seo');
-	}
+    public function seo()
+    {
+        return View::make('settings.seo');
+    }
 
-	public function buying() {
-		return View::make('settings.buying');
-	}
-
+    public function buying()
+    {
+        return View::make('settings.buying');
+    }
 }
