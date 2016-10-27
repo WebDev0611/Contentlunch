b0ff8cd884c4a69f2aa716ec3afb9791575d9773
Andre Castelo <andrecastelo@gmail.com>
2016-10-26 23:45:26 -0200

    added method to get the name of the file

--- app/Attachment.php
+++ app/Attachment.php
@@ -7,9 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 class Attachment extends Model
 {
     public $fillable = [ 'filepath', 'filename', 'type', 'extension', 'mime' ];
+    public $appends = [ 'name' ];
 
     public function contents()
     {
         return $this->hasOne('App\Content');
+    }
+
+    public function getNameAttribute()
+    {
+        $pattern = "/attachment\/(\d+\/(files|images)|_tmp)\//";
+
+        return preg_replace($pattern, '', $this->filepath);
     }
 }
