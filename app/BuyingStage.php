6aa7fb689d9d4e4c251b6011a7dbcc76d1c804db
Andre Castelo <andrecastelo@gmail.com>
2016-10-28 18:35:09 -0200

    buying stage formatting

--- app/BuyingStage.php
+++ app/BuyingStage.php
@@ -1,17 +1,23 @@
-<?php namespace App;
+<?php
+
+namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
 class BuyingStage extends Model
 {
+    public static function dropdown()
+    {
+        // - Create Related Content Drop Down Data
+        $buyingStageDropdown = ['' => '-- Select a Buying Stage --'];
+        $buyingStageDropdown += self::select('id', 'name')
+            ->orderBy('name', 'asc')
+            ->distinct()
+            ->lists('name', 'id')
+            ->toArray();
 
-	public static function dropdown()
-	{
-		// - Create Related Content Drop Down Data
-		$stageddd = ['' => '-- Select a Buying Stage --'];
-		$stageddd += BuyingStage::select('id','name')->orderBy('name', 'asc')->distinct()->lists('name', 'id')->toArray();
-		return $stageddd;
-	}
+        return $buyingStageDropdown;
+    }
 
     public function __toString()
     {
