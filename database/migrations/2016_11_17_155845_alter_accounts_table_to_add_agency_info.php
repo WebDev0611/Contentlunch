1d1d4b6496946e15d3dcf1963087af1d571cf66d
André Castelo <andrecastelo@gmail.com>
2016-11-17 14:07:54 -0200

    altered accounts table to support agencies

---
+++ database/migrations/2016_11_17_155845_alter_accounts_table_to_add_agency_info.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterAccountsTableToAddAgencyInfo extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('accounts', function (Blueprint $table) {
+            $table->integer('account_type_id')->after('name')->unsigned()->default('1');
+            $table->integer('parent_account_id')->after('account_type_id')->unsigned()->nullable();
+
+            $table->foreign('account_type_id', 'account_type_fk')->references('id')->on('account_types');
+            $table->foreign('parent_account_id', 'parent_account_fk')->references('id')->on('accounts');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('accounts', function (Blueprint $table) {
+            $table->dropForeign('account_type_fk');
+            $table->dropForeign('parent_account_fk');
+            $table->dropColumn('account_type_id');
+            $table->dropColumn('parent_account_id');
+        });
+    }
+}
