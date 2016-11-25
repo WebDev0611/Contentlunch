6cc6320ba73f993bfc0225b3e1861328822e3705
André Castelo <andrecastelo@gmail.com>
2016-11-25 14:54:38 -0200

    altering factories to account for the new fields

--- database/factories/BuyingStageFactory.php
+++ database/factories/BuyingStageFactory.php
@@ -3,5 +3,9 @@
 $factory->define(App\BuyingStage::class, function (Faker\Generator $faker) {
     return [
         'name' => ucwords($faker->word) . ' Stage',
+        'description' => $faker->realText(200),
+        'account_id' => function (array $buyingStage) {
+            return factory(App\Account::class)->create()->id;
+        }
     ];
 });
--- database/factories/PersonaFactory.php
+++ database/factories/PersonaFactory.php
@@ -3,5 +3,9 @@
 $factory->define(App\Persona::class, function (Faker\Generator $faker) {
     return [
         'name' => ucwords($faker->word),
+        'description' => $faker->realText(200),
+        'account_id' => function (array $buyingStage) {
+            return factory(App\Account::class)->create()->id;
+        }
     ];
 });
