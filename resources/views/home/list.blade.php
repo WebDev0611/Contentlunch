462e534bbac7f4171cf172f091a1cd22deb4145c
André Castelo <andrecastelo@gmail.com>
2016-11-30 17:07:36 -0200

    added invite team member modal

--- resources/views/home/list.blade.php
+++ resources/views/home/list.blade.php
@@ -106,7 +106,7 @@
                     <div class="panel-header">
                         <h4 class="panel-sidebar-title-secondary">
                             Team Members
-                            <a href="#">
+                            <a href="#" data-toggle="modal" data-target="#modal-invite-teammember">
                                 INVITE
                                 <i class="icon-edit-user"></i>
                             </a>
@@ -116,6 +116,38 @@
 
             </div>
 
+        </div>
+    </div>
+</div>
+
+<div class="modal fade"
+    id="modal-invite-teammember"
+    tabindex="-1"
+    role="dialog"
+    aria-labelledby="Invite Client">
+
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="myModalLabel">Invite Team Member</h4>
+            </div>
+            <div class="modal-body">
+
+                <div class="inner">
+                    <p class="intro">Invite team members to ContentLaunch to share content, plans and more.</p>
+
+                    <div class="input-form-group">
+                        <label for="#">Invite</label>
+                        <input type="text" class="input" placeholder="One or more e-mail addresses">
+                    </div>
+
+                    <button class="button button-extend text-uppercase">
+                        Send Invitation
+                    </button>
+
+                </div>
+            </div>
         </div>
     </div>
 </div>
