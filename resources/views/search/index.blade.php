2eb9f1bf5f1e242e831856e2e8db6212ca764269
André Castelo <andrecastelo@gmail.com>
2016-12-02 23:59:41 -0200

    added tab control on the search results page

--- resources/views/search/index.blade.php
+++ resources/views/search/index.blade.php
@@ -10,16 +10,33 @@
                     <div class="panel-header">
                         <ul class="panel-tabs spacing">
                             <li class="active">
-                                <a href="#">Content</a>
+                                <a href="#content" data-toggle='tab'>Content</a>
                             </li>
-                            <li><a href="#">Users</a></li>
-                            <li><a href="#">Tasks</a></li>
+                            <li><a href="#users" data-toggle='tab'>Users</a></li>
+                            <li><a href="#tasks" data-toggle='tab'>Tasks</a></li>
                         </ul>
                     </div>
                     <div class="panel-body">
-                        Panel body.
-                    </div>
+                        <div class="tab-content">
+                            <div role="tabpanel" class="tab-pane active" id='content'>
+                                <div class="panel-container">
+                                    content
+                                </div>
+                            </div>
 
+                            <div role="tabpanel" class="tab-pane" id='users'>
+                                <div class="panel-container">
+                                    users
+                                </div>
+                            </div>
+
+                            <div role="tabpanel" class="tab-pane" id='tasks'>
+                                <div class="panel-container">
+                                    tasks
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
