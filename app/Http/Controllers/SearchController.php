0588442947536f9cbe9079549b248aae092c8649
André Castelo <andrecastelo@gmail.com>
2016-12-05 18:54:34 -0200

    implemented searching of resources in the currently selected account

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Validator;
 use DB;
 
+use App\Account;
 use App\Content;
 use App\User;
 use App\Task;
@@ -13,16 +14,17 @@ use App\Idea;
 
 class SearchController extends Controller
 {
+    public function __construct(Request $request)
+    {
+        $this->selectedAccount = Account::selectedAccount();
+    }
+
     public function index(Request $request)
     {
         $validation = $this->validateRequest($request);
 
         if ($validation->fails()) {
-            return redirect('/')->with([
-                'flash_message' => 'Invalid search term',
-                'flash_message_type' => 'danger',
-                'flash_message_important' => true,
-            ]);
+            return $this->redirectFailedValidation();
         }
 
         $searchTerm = $request->input('search');
@@ -32,9 +34,19 @@ class SearchController extends Controller
             'users' => $this->searchUsers($searchTerm),
             'tasks' => $this->searchTasks($searchTerm),
             'ideas' => $this->searchIdeas($searchTerm),
+            'searchTerm' => $searchTerm,
         ];
 
         return view('search.index', $data);
+    }
+
+    private function redirectFailedValidation()
+    {
+        return redirect('/')->with([
+            'flash_message' => 'Invalid search term',
+            'flash_message_type' => 'danger',
+            'flash_message_important' => true,
+        ]);
     }
 
     private function validateRequest(Request $request)
@@ -46,27 +58,36 @@ class SearchController extends Controller
 
     private function searchContent($term)
     {
-        // Search content the user has access to
-        // return Content::where('title', 'like', '%' . $term . '%')
-        //     ->orWhere('body', 'like', '%' . $term . '%')
-        //     ->get();
-        return Content::all();
+        return $this->selectedAccount
+            ->contents()
+            ->where('title', 'like', '%' . $term . '%')
+            ->orWhere('body', 'like', '%' . $term . '%')
+            ->get();
     }
 
     private function searchUsers($term)
     {
-        // return User::where('name', 'like', '%' . $term . '%')
-        //     ->get();
-        return User::all();
+        return $this->selectedAccount
+            ->users()
+            ->where('name', 'like', '%' . $term . '%')
+            ->get();
     }
 
     private function searchTasks($term)
     {
-        return Task::all();
+        return $this->selectedAccount
+            ->tasks()
+            ->where('name', 'like', '%' . $term . '%')
+            ->orWhere('explanation', 'like', '%' . $term . '%')
+            ->get();
     }
 
     private function searchIdeas($term)
     {
-        return Idea::all();
+        return $this->selectedAccount
+            ->ideas()
+            ->where('name', 'like', '%' . $term . '%')
+            ->orWhere('text', 'like', '%' . $term . '%')
+            ->get();
     }
 }
