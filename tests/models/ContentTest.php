f19921e67f363254b446a9e55847865691f972d4
André Castelo <andrecastelo@gmail.com>
2016-12-06 12:57:03 -0200

    added setup method for the content test

--- tests/models/ContentTest.php
+++ tests/models/ContentTest.php
@@ -4,8 +4,20 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
+use App\Content;
+
 class ContentTest extends TestCase
 {
+    public function setUp()
+    {
+        parent::setUp();
+        $this->account = factory(App\Account::class)->create();
+        $this->content = factory(App\Content::class)->create([
+            'title' => 'Test Title',
+            'body' => '<p>test body content</p>',
+        ]);
+    }
+
     /**
      * A basic test example.
      *
