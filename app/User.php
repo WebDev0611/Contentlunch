ca029baeefb7e8e251c07aedb6da76d34e32f247
André Castelo <andrecastelo@gmail.com>
2016-12-27 16:37:44 -0300

    default profile image now set as constant in the User class

--- app/User.php
+++ app/User.php
@@ -30,6 +30,7 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
+    const DEFAULT_PROFILE_IMAGE = '/images/avatar.jpg';
 
     public function connections()
     {
