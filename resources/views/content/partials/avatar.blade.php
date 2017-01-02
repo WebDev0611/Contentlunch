ad408a8bed6902203a380fd304685bf6ca0afbc8
André Castelo <andrecastelo@gmail.com>
2017-01-02 18:44:48 -0300

    fixes author bug

--- resources/views/content/partials/avatar.blade.php
+++ resources/views/content/partials/avatar.blade.php
@@ -1,5 +1,6 @@
 @php
     $author = $content->author();
     $authorName = $author ? $author->name : 'No author specified';
+    $profileImage = $author ? $author->present()->profile_image : \App\User::DEFAULT_PROFILE_IMAGE;
 @endphp
-<img src="{{ $author->present()->profile_image }}" class='create-image' alt="{{ $authorName }}" title="{{ $authorName }}">
+<img src="{{ $profileImage }}" class='create-image' alt="{{ $authorName }}" title="{{ $authorName }}">
