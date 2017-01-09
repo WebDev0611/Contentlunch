68428604dc5806c628ebfc6ec93151e6be050926
André Castelo <andrecastelo@gmail.com>
2017-01-09 18:30:16 -0300

    added missing asset type - case study

--- database/seeds/WriterAccessPriceSeeder.php
+++ database/seeds/WriterAccessPriceSeeder.php
@@ -7,6 +7,7 @@ class WriterAccessPriceSeeder extends Seeder{
 
     public function run()
     {
+        DB::table('writer_access_prices')->truncate();
         DB::table('writer_access_prices')->insert([
 
             // Blog Post
@@ -44,22 +45,40 @@ class WriterAccessPriceSeeder extends Seeder{
 
 
             // White paper
-            ['asset_type_id' => 1, 'writer_level' => 6, 'wordcount' => 2000, 'fee' => 500],
-            ['asset_type_id' => 1, 'writer_level' => 6, 'wordcount' => 3000, 'fee' => 700],
-            ['asset_type_id' => 1, 'writer_level' => 6, 'wordcount' => 4000, 'fee' => 900],
-            ['asset_type_id' => 1, 'writer_level' => 6, 'wordcount' => 5000, 'fee' => 1100],
+            ['asset_type_id' => 13, 'writer_level' => 6, 'wordcount' => 2000, 'fee' => 500],
+            ['asset_type_id' => 13, 'writer_level' => 6, 'wordcount' => 3000, 'fee' => 700],
+            ['asset_type_id' => 13, 'writer_level' => 6, 'wordcount' => 4000, 'fee' => 900],
+            ['asset_type_id' => 13, 'writer_level' => 6, 'wordcount' => 5000, 'fee' => 1100],
 
-            ['asset_type_id' => 1, 'writer_level' => 5, 'wordcount' => 2000, 'fee' => 400],
-            ['asset_type_id' => 1, 'writer_level' => 5, 'wordcount' => 3000, 'fee' => 600],
-            ['asset_type_id' => 1, 'writer_level' => 5, 'wordcount' => 4000, 'fee' => 800],
-            ['asset_type_id' => 1, 'writer_level' => 5, 'wordcount' => 5000, 'fee' => 1000],
+            ['asset_type_id' => 13, 'writer_level' => 5, 'wordcount' => 2000, 'fee' => 400],
+            ['asset_type_id' => 13, 'writer_level' => 5, 'wordcount' => 3000, 'fee' => 600],
+            ['asset_type_id' => 13, 'writer_level' => 5, 'wordcount' => 4000, 'fee' => 800],
+            ['asset_type_id' => 13, 'writer_level' => 5, 'wordcount' => 5000, 'fee' => 1000],
 
-            ['asset_type_id' => 1, 'writer_level' => 4, 'wordcount' => 2000, 'fee' => 350],
-            ['asset_type_id' => 1, 'writer_level' => 4, 'wordcount' => 3000, 'fee' => 550],
-            ['asset_type_id' => 1, 'writer_level' => 4, 'wordcount' => 4000, 'fee' => 750],
-            ['asset_type_id' => 1, 'writer_level' => 4, 'wordcount' => 5000, 'fee' => 950],
+            ['asset_type_id' => 13, 'writer_level' => 4, 'wordcount' => 2000, 'fee' => 350],
+            ['asset_type_id' => 13, 'writer_level' => 4, 'wordcount' => 3000, 'fee' => 550],
+            ['asset_type_id' => 13, 'writer_level' => 4, 'wordcount' => 4000, 'fee' => 750],
+            ['asset_type_id' => 13, 'writer_level' => 4, 'wordcount' => 5000, 'fee' => 950],
+
 
             // Case Study
+            ['asset_type_id' => 1, 'writer_level' => 6, 'wordcount' => 500, 'fee' => 250 ],
+            ['asset_type_id' => 1, 'writer_level' => 6, 'wordcount' => 750, 'fee' => 350 ],
+            ['asset_type_id' => 1, 'writer_level' => 6, 'wordcount' => 1000, 'fee' => 450 ],
+            ['asset_type_id' => 1, 'writer_level' => 6, 'wordcount' => 1200, 'fee' => 550 ],
+
+            ['asset_type_id' => 1, 'writer_level' => 5, 'wordcount' => 500, 'fee' => 200 ],
+            ['asset_type_id' => 1, 'writer_level' => 5, 'wordcount' => 750, 'fee' => 300 ],
+            ['asset_type_id' => 1, 'writer_level' => 5, 'wordcount' => 1000, 'fee' => 400 ],
+            ['asset_type_id' => 1, 'writer_level' => 5, 'wordcount' => 1200, 'fee' => 500 ],
+
+            ['asset_type_id' => 1, 'writer_level' => 4, 'wordcount' => 500, 'fee' => 150 ],
+            ['asset_type_id' => 1, 'writer_level' => 4, 'wordcount' => 750, 'fee' => 250 ],
+            ['asset_type_id' => 1, 'writer_level' => 4, 'wordcount' => 1000, 'fee' => 350 ],
+            ['asset_type_id' => 1, 'writer_level' => 4, 'wordcount' => 1200, 'fee' => 450 ],
+
+
+            // Email
             ['asset_type_id' => 30, 'writer_level' => 6, 'wordcount' => 500, 'fee' => 250],
             ['asset_type_id' => 30, 'writer_level' => 6, 'wordcount' => 750, 'fee' => 350],
             ['asset_type_id' => 30, 'writer_level' => 6, 'wordcount' => 1000, 'fee' => 450],
@@ -76,7 +95,7 @@ class WriterAccessPriceSeeder extends Seeder{
             ['asset_type_id' => 30, 'writer_level' => 4, 'wordcount' => 1200, 'fee' => 450],
 
 
-            // Email
+            // Website page
             ['asset_type_id' => 26, 'writer_level' => 6, 'wordcount' => 200, 'fee' => 55],
             ['asset_type_id' => 26, 'writer_level' => 6, 'wordcount' => 400, 'fee' => 90],
             ['asset_type_id' => 26, 'writer_level' => 6, 'wordcount' => 600, 'fee' => 130],
@@ -93,21 +112,23 @@ class WriterAccessPriceSeeder extends Seeder{
             ['asset_type_id' => 26, 'writer_level' => 4, 'wordcount' => 800, 'fee' => 130],
 
 
-            // Website Page
+            // Press Release
             ['asset_type_id' => 8, 'writer_level' => 6, 'wordcount' => 200, 'fee' => 70],
             ['asset_type_id' => 8, 'writer_level' => 6, 'wordcount' => 300, 'fee' => 80],
             ['asset_type_id' => 8, 'writer_level' => 6, 'wordcount' => 400, 'fee' => 90],
             ['asset_type_id' => 8, 'writer_level' => 6, 'wordcount' => 500, 'fee' => 100],
+
             ['asset_type_id' => 8, 'writer_level' => 5, 'wordcount' => 200, 'fee' => 60],
             ['asset_type_id' => 8, 'writer_level' => 5, 'wordcount' => 300, 'fee' => 70],
             ['asset_type_id' => 8, 'writer_level' => 5, 'wordcount' => 400, 'fee' => 80],
             ['asset_type_id' => 8, 'writer_level' => 5, 'wordcount' => 500, 'fee' => 90],
+
             ['asset_type_id' => 8, 'writer_level' => 4, 'wordcount' => 200, 'fee' => 55],
             ['asset_type_id' => 8, 'writer_level' => 4, 'wordcount' => 300, 'fee' => 65],
             ['asset_type_id' => 8, 'writer_level' => 4, 'wordcount' => 400, 'fee' => 75],
             ['asset_type_id' => 8, 'writer_level' => 4, 'wordcount' => 500, 'fee' => 85],
 
-            // Press Release
+            // Newsletter
             ['asset_type_id' => 31, 'writer_level' => 6, 'wordcount' => 400, 'fee' => 90],
             ['asset_type_id' => 31, 'writer_level' => 6, 'wordcount' => 600, 'fee' => 110],
             ['asset_type_id' => 31, 'writer_level' => 6, 'wordcount' => 800, 'fee' => 135],
@@ -120,7 +141,7 @@ class WriterAccessPriceSeeder extends Seeder{
             ['asset_type_id' => 31, 'writer_level' => 4, 'wordcount' => 600, 'fee' => 90],
             ['asset_type_id' => 31, 'writer_level' => 4, 'wordcount' => 800, 'fee' => 110],
 
-            // Newsletter
+            // Product Description
             ['asset_type_id' => 32, 'writer_level' => 6, 'wordcount' => 1000, 'fee' => 250],
             ['asset_type_id' => 32, 'writer_level' => 6, 'wordcount' => 1500, 'fee' => 350],
             ['asset_type_id' => 32, 'writer_level' => 6, 'wordcount' => 2000, 'fee' => 450],
@@ -136,7 +157,7 @@ class WriterAccessPriceSeeder extends Seeder{
             ['asset_type_id' => 32, 'writer_level' => 4, 'wordcount' => 2000, 'fee' => 375],
             ['asset_type_id' => 32, 'writer_level' => 4, 'wordcount' => 2500, 'fee' => 475],
 
-            // Product Description
+            // Script (Video)
             ['asset_type_id' => 9, 'writer_level' => 6, 'wordcount' => 50, 'fee' => 40],
             ['asset_type_id' => 9, 'writer_level' => 6, 'wordcount' => 150, 'fee' => 60],
             ['asset_type_id' => 9, 'writer_level' => 6, 'wordcount' => 300, 'fee' => 90],
@@ -152,7 +173,7 @@ class WriterAccessPriceSeeder extends Seeder{
             ['asset_type_id' => 9, 'writer_level' => 4, 'wordcount' => 300, 'fee' => 75],
             ['asset_type_id' => 9, 'writer_level' => 4, 'wordcount' => 500, 'fee' => 90],
 
-            // Script (Video)
+            // Speech
             ['asset_type_id' => 10, 'writer_level' => 6, 'wordcount' => 2000, 'fee' => 400],
             ['asset_type_id' => 10, 'writer_level' => 6, 'wordcount' => 3000, 'fee' => 500],
             ['asset_type_id' => 10, 'writer_level' => 6, 'wordcount' => 4000, 'fee' => 600],
@@ -170,17 +191,6 @@ class WriterAccessPriceSeeder extends Seeder{
 
         ]);
 
-
-/*
-    foreach ($writerAccessPrices as $row) {
-        $writerAccessPrice = new WriterAccessPrice;
-        $writerAccessPrice->asset_type_id = $row['asset_type_id'];
-        $writerAccessPrice->writer_level = $row['writer_level'];
-        $writerAccessPrice->wordcount = $row['wordcount'];
-        $writerAccessPrice->fee = $row['fee'];
-        $writerAccessPrice->updateUniques();
-    }*/
-
-    $this->command->info('Added Writer Access Prices');
-  }
+        $this->command->info('Added Writer Access Prices');
+    }
 }
