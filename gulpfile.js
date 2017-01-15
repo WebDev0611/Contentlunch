17c96e519b66ea7bbadcbecc3e03317a41ea37cf
André Castelo <andrecastelo@gmail.com>
2017-01-15 21:19:14 -0200

    streamlining gulpfile, removing duplicates

--- gulpfile.js
+++ gulpfile.js
@@ -58,101 +58,44 @@ elixir(function (mix) {
 
             /* plan module scripts */
             /* trends */
-            .babel(['resources/assets/js/plan/topic.js'],"public/js/topic.js")
-            .babel([
-                'resources/assets/js/models/trends.js',
-                'resources/assets/js/views/trends.js',
-                'resources/assets/js/collections/trends.js',
-                'resources/assets/js/plan/trends.js'
-            ],"public/js/trends.js")
+            .babel('resources/assets/js/plan/topic.js', "public/js/topic.js")
+            .babel('resources/assets/js/plan/trends.js', "public/js/trends.js")
 
             /* Avatar View */
-            .babel([ 'resources/assets/js/views/avatar.js' ], 'public/js/avatar_view.js')
+            .babel('resources/assets/js/views/avatar.js', 'public/js/avatar_view.js')
 
             /* Onboarding */
-            .babel([
-                'resources/assets/js/onboarding/*.js'
-            ], 'public/js/onboarding.js')
+            .babel('resources/assets/js/onboarding/*.js', 'public/js/onboarding.js')
 
             /* ideas */
+            .babel('resources/assets/js/plan/ideas.js', "public/js/ideas.js")
             .babel([
-                'resources/assets/js/models/ideas.js',
-                'resources/assets/js/collections/ideas.js',
-                'resources/assets/js/views/ideas.js',
-                'resources/assets/js/plan/ideas.js',
-            ],"public/js/ideas.js")
-
-            .babel([
-                'resources/assets/js/models/ideas.js',
                 'resources/assets/js/plan/editor.js',
                 'resources/assets/js/plan/idea_collaborators.js',
             ], "public/js/idea_editor.js")
 
             /* influencers scripts */
-            .babel([
-                'resources/assets/js/collaborate/influencers.js',
-            ], "public/js/influencers.js")
+            .babel('resources/assets/js/collaborate/influencers.js', "public/js/influencers.js")
 
             /* campaign scripts */
-            .babel([
-                'resources/assets/js/models/campaigns.js',
-                'resources/assets/js/collections/campaigns.js',
-                'resources/assets/js/views/campaign.js',
-                'resources/assets/js/campaign/campaign.js'
-            ],"public/js/campaign.js")
+            .babel('resources/assets/js/campaign/campaign.js',"public/js/campaign.js")
 
             /* calendar scripts */
-            .babel([
-                'resources/assets/js/models/campaigns.js',
-                'resources/assets/js/models/ideas.js',
-                'resources/assets/js/models/content.js',
-                'resources/assets/js/collections/campaigns.js',
-                'resources/assets/js/collections/ideas.js',
-                'resources/assets/js/collections/content.js',
-                'resources/assets/js/calendar/calendar.js'
-            ],"public/js/calendar.js")
-
-            .babel([
-                'resources/assets/js/models/campaigns.js',
-                'resources/assets/js/calendar/campaign-calendar.js'
-            ],"public/js/campaign-calendar.js")
+            .babel('resources/assets/js/calendar/calendar.js', "public/js/calendar.js")
+            .babel('resources/assets/js/calendar/campaign-calendar.js', "public/js/campaign-calendar.js")
 
             /* home area scripts */
-            .babel([
-                'resources/assets/js/models/ideas.js',
-                'resources/assets/js/models/campaigns.js',
-                'resources/assets/js/models/tasks.js',
-                'resources/assets/js/collections/ideas.js',
-                'resources/assets/js/collections/campaigns.js',
-                'resources/assets/js/collections/tasks.js',
-                'resources/assets/js/views/ideas.js',
-                'resources/assets/js/views/team_member_invite_modal.js',
-                'resources/assets/js/dashboard/dashboard.js',
-            ],"public/js/dashboard.js")
-
-            .babel([
-                'resources/assets/js/models/ideas.js',
-                'resources/assets/js/models/campaigns.js',
-                'resources/assets/js/collections/ideas.js',
-                'resources/assets/js/collections/campaigns.js',
-                'resources/assets/js/views/ideas.js',
-                'resources/assets/js/dashboard/performance.js'
-            ],"public/js/performance.js")
+            .babel('resources/assets/js/dashboard/dashboard.js', "public/js/dashboard.js")
+            .babel('resources/assets/js/dashboard/performance.js', "public/js/performance.js")
 
             /* settings scripts */
-            .babel([
-                'resources/assets/js/settings/*.js'
-            ], 'public/js/settings.js')
+            .babel('resources/assets/js/settings/*.js', 'public/js/settings.js')
 
             /* task editor */
-            .babel([
-                'resources/assets/js/task/*.js'
-            ], 'public/js/task_editor.js')
+            .babel('resources/assets/js/task/*.js', 'public/js/task_editor.js')
 
             /* twitter scripts */
-            .babel([
-                'resources/assets/js/collaborate/twitter.js'
-            ], 'public/js/twitter.js')
+            .babel('resources/assets/js/collaborate/twitter.js', 'public/js/twitter.js')
 
             /* tiny mce assets */
             .copy('./bower_components/tinymce/themes', "public/js/themes")
