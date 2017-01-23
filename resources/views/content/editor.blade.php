9ac8d1400da5e327da3f316c666a52dee553255e
André Castelo <andrecastelo@gmail.com>
2017-01-23 09:09:31 -0200

    content status icons now show up properly

--- resources/views/content/editor.blade.php
+++ resources/views/content/editor.blade.php
@@ -81,11 +81,14 @@
                 <!-- Panel Container -->
                 <div class="panel-container padded relative">
                     <!-- Stages widget -->
+                    @php
+                        $status = $content->present()->contentStatusIcon();
+                    @endphp
                     <ul class="list-unstyled list-stages list-stages-side">
-                        <li><i class="icon-connect"></i></li>
-                        <li><i class="icon-alert"></i></li>
-                        <li class="active"><i class="icon-edit"></i></li>
-                        <li class="active"><i class="icon-idea"></i></li>
+                        <li @if ($status >= 4) class='active' @endif><i class="icon-connect"></i></li>
+                        <li @if ($status >= 3) class='active' @endif><i class="icon-alert"></i></li>
+                        <li @if ($status >= 2) class='active' @endif><i class="icon-edit"></i></li>
+                        <li @if ($status >= 1) class='active' @endif><i class="icon-idea"></i></li>
                     </ul>
 
                     <div class="inner">
