1f8456a8cf13aefd3e028133d6d6795475980ec6
antesko <contact@antesko.xyz>
2017-01-25 17:24:43 +0100

    adding parameters to Hubspot integration and fixing bugs

--- app/libraries/Connections/API/HubspotAPI.php
+++ app/libraries/Connections/API/HubspotAPI.php
@@ -19,6 +19,8 @@ class HubspotAPI {
 
     protected $base_url = 'https://api.hubapi.com';
 
+    private $blog;
+
     public function __construct ($content = null, $connection = null) {
         $this->client = null;
         $this->content = $content;
@@ -33,7 +35,7 @@ class HubspotAPI {
 
         $refreshedSettings = $auth->refreshToken($settings->refresh_token);
         $this->saveConnectionSettings(json_encode($refreshedSettings));
-        
+
         // New Hubspot Factory instance
         $hubspot = new HubspotFactory([
             'key' => $settings->access_token,
@@ -44,28 +46,26 @@ class HubspotAPI {
             false
         );
 
-        // Return array
+        // Response array
         $response = [
             'success' => false,
             'response' => []
         ];
-
+        
         // Get the blog we're posting to
         // Note: user can have multiple blogs, but for now we'll get the first one
         $blogs = ($hubspot->blogs()->all()->getBody()->getContents());
-        $blog = json_decode($blogs)->objects[0];
-
-        //var_dump(json_decode($hubspot->blogPosts()->all()->getBody()->getContents()));
+        $this->blog = json_decode($blogs)->objects[0];
 
         try {
-            // TODO: load all posting parameters
-            $createResponse = $hubspot->blogPosts()->create([
-                'content_group_id' => $blog->id,
-                'name' => $this->content->title
-            ]);
+            $createResponse = $hubspot->blogPosts()->create($this->preparePostingData());
 
-            // If response status code starts with "2" (e.g. 200) , we're OK
-            if (substr($createResponse->getStatusCode(), 0, 1) == '2') {
+            if ($createResponse->getStatusCode() == '201') {
+
+                // TODO: If we scheduled the date for publishing, we have to make another call to the API
+                // $blogPostId = json_decode($createResponse->getBody()->getContents())->id;
+                // $hubspot->blogPosts()->publishAction($blogPostId, 'schedule-publish');
+
                 $response = [
                     'success' => true,
                     'response' => json_encode($createResponse),
@@ -83,6 +83,34 @@ class HubspotAPI {
     public function saveConnectionSettings ($jsonEncodedSettings) {
         $this->connection->settings = $jsonEncodedSettings;
         $this->connection->save();
+    }
+
+    /**
+     * Prepares input data for posting to Hubspot
+     * @return array
+     */
+    private function preparePostingData () {
+
+        $isImageSet = Input::has('images');
+        $featuredImage = $isImageSet ? $this->getMediaUrls()->shift() : '';
+
+        return [
+            'content_group_id' => $this->blog->id,
+            'name' => $this->content->title,
+            'post_body' => $this->content->body,
+            //'keywords' => $this->content->meta_keywords, // Keywords are not supported on test portal
+            'meta_description' => $this->content->meta_description,
+            'featured_image' => $featuredImage,
+            'use_featured_image' => $isImageSet,
+            //'publish_date' => '' // TODO: schedule date for posting
+        ];
+    }
+
+    private function getMediaUrls () {
+        return $this->content
+            ->attachments
+            ->where('type', 'image')
+            ->pluck('filename');
     }
 
 }
