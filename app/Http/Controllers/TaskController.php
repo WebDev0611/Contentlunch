f53b968b5280fd9149aca127be018a753682f843
André Castelo <andrecastelo@gmail.com>
2017-01-26 23:54:10 -0200

    small refactoring

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -176,12 +176,12 @@ class TaskController extends Controller
             'user_id' => Auth::id()
         ])->first();
 
-        $task->name = $request->input('name');
-        $task->explanation = $request->input('explanation');
-        $task->start_date = $request->input('start_date');
-        $task->due_date = $request->input('due_date');
-
-        $task->save();
+        $task->update([
+            'name' => $request->input('name'),
+            'explanation' => $request->input('explanation'),
+            'start_date' => $request->input('start_date'),
+            'due_date' => $request->input('due_date'),
+        ]);
 
         $this->saveAssignedUsers($request, $task);
 
