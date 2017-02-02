f0d488a3a7a999084b6eac0f9a2b13b107315972
André Castelo <andrecastelo@gmail.com>
2017-02-02 17:38:26 -0200

    added header to the campaign editor tab

--- resources/views/campaign/index.blade.php
+++ resources/views/campaign/index.blade.php
@@ -9,55 +9,13 @@
         <!-- Main Pane -->
         <div class="panel-main">
 
-            <!-- Panel Header -->
-            <div class="panel-header" id="create-campaign-form">
-                <div class="panel-options">
-                    <div class="row">
-                        <div class="col-md-6">
-                            <h4>Campaign editor</h4>
-                        </div>
-                        <div class="col-md-6 text-right">
-                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                            <div class="head-actions">
-                                <button
-                                    type="button"
-                                    class="button button-outline-secondary button-small delimited"
-                                    id="save-campaign-button">SAVE</button>
-
-                                <div class="btn-group">
-                                    <button
-                                        type="button"
-                                        class="button button-small">DONE</button>
-
-                                    <button
-                                        type="button"
-                                        class="button button-small dropdown-toggle"
-                                        data-toggle="dropdown"
-                                        aria-haspopup="true"
-                                        aria-expanded="false">
-
-                                        <span class="caret"></span>
-                                        <span class="sr-only">Toggle Dropdown</span>
-                                    </button>
-
-                                    <ul class="dropdown-menu dropdown-menu-right">
-                                        <li><a href="#">Park</a></li>
-                                        <li><a href="#">Delete</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div> <!-- End Panel Header -->
-
             <!-- Content Tabs -->
             <ul class="panel-tabs centered even withborder">
                 <li class="active">
                     <a href="#contenttab-content" role="tab" data-toggle="tab">Content</a>
                 </li>
                 <li>
-                    <a href="#contenttab-campaign" role="tab" data-toggle="tab">Campaign Info</a>
+                    <a href="#contenttab-campaign" role="tab" data-toggle="tab">Campaign Editor</a>
                 </li>
             </ul>
 
@@ -74,6 +32,7 @@
 
                     <!-- Tab2: Campaign Info -->
                     <div role="tabpanel" class="tab-pane" id="contenttab-campaign">
+                        @include('campaign.partials.header')
                         @include('campaign.partials.campaign_tab')
                     </div>
 
---
+++ resources/views/campaign/partials/header.blade.php
@@ -0,0 +1,34 @@
+<div class="panel-options">
+    <div class="row">
+        <div class="col-md-offset-6 col-md-6 text-right">
+            <div class="head-actions">
+                <button
+                    type="button"
+                    class="button button-outline-secondary button-small delimited"
+                    id="save-campaign-button">SAVE</button>
+
+                <div class="btn-group">
+                    <button
+                        type="button"
+                        class="button button-small">DONE</button>
+
+                    <button
+                        type="button"
+                        class="button button-small dropdown-toggle"
+                        data-toggle="dropdown"
+                        aria-haspopup="true"
+                        aria-expanded="false">
+
+                        <span class="caret"></span>
+                        <span class="sr-only">Toggle Dropdown</span>
+                    </button>
+
+                    <ul class="dropdown-menu dropdown-menu-right">
+                        <li><a href="#">Park</a></li>
+                        <li><a href="#">Delete</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
