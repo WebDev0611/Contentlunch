5cb818e7ec9f29254b40e4d6a99c5fae4c923c89
André Castelo <andrecastelo@gmail.com>
2017-02-02 14:07:27 -0200

    simplifying campaign templates

--- resources/views/campaign/index.blade.php
+++ resources/views/campaign/index.blade.php
@@ -64,124 +64,9 @@
 
             <!-- Tab1: Content -->
             <div role="tabpanel" class="tab-pane active" id="contenttab-content">
-
-
               <!-- Panel Ready to be Published -->
-              <div class="content-panel">
-                <p class="panel-head"><span class="picto"><i class="icon-alert"></i></span> READY TO BE PUBLISHED</p>
-
-                <ul class="list-unstyled list-content bordered">
-                  <li class="list-alert">
-                    <div class="list-avatar">
-                      <div class="user-avatar">
-                        <img src="/images/cl-avatar2.png">
-                      </div>
-                    </div>
-                    <div class="list-title">
-                      <a href="#">
-                        <p>Post 16 social postings on woman rights and movements around the world</p>
-                        <p class="small">15 DAYS AGO <span class="delimit">&middot;</span>  NEXT TASK: <strong>PUBLISH</strong></p>
-                      </a>
-                    </div>
-                    <div class="list-team">
-
-                    </div>
-                    <div class="list-type">
-                      <i class="icon-type-blog"></i>
-                    </div>
-                  </li>
-                  <li>
-                    <div class="list-avatar">
-                      <div class="user-avatar">
-                        <img src="/images/cl-avatar2.png">
-                      </div>
-                    </div>
-                    <div class="list-title">
-                      <a href="#">
-                        <p>Write blog post on online banking</p>
-                        <p class="small">15 DAYS AGO <span class="delimit">&middot;</span>  NEXT TASK: <strong>PUBLISH</strong></p>
-                      </a>
-                    </div>
-                    <div class="list-type">
-                      <i class="icon-type-facebook"></i>
-                    </div>
-                  </li>
-                  <li>
-                    <div class="list-avatar">
-                      <div class="user-avatar">
-                        <img src="/images/cl-avatar2.png">
-                      </div>
-                    </div>
-                    <div class="list-title">
-                      <a href="#">
-                        <p>Post 16 social postings on woman rights and movements around the world</p>
-                        <p class="small">15 DAYS AGO <span class="delimit">&middot;</span>  NEXT TASK: <strong>PUBLISH</strong></p>
-                      </a>
-                    </div>
-                    <div class="list-team">
-
-                    </div>
-                    <div class="list-type">
-                      <i class="icon-type-blog"></i>
-                    </div>
-                  </li>
-                </ul>
-
-
-              </div>
-
-
-              <!-- Panel Being Edited / Written -->
-              <div class="content-panel">
-                <p class="panel-head"><span class="picto"><i class="icon-edit"></i></span> BEING EDITED / WRITTEN</p>
-
-                <ul class="list-unstyled list-content bordered">
-                  <li class="list-external">
-                    <div class="progressbar" style="width: 35%"></div>
-                    <div class="list-avatar">
-                      <div class="user-avatar">
-                        <img src="/images/cl-avatar2.png">
-                      </div>
-                    </div>
-                    <div class="list-title">
-                      <a href="#">
-                        <p>Write blog post on online banking <span class="badge-external">External</span></p>
-                        <p class="small">15 DAYS AGO <span class="delimit">&middot;</span>  NEXT TASK: <strong>EDIT</strong></p>
-                      </a>
-                    </div>
-                    <div class="list-team">
-
-                    </div>
-                    <div class="list-type">
-                      <i class="icon-type-blog"></i>
-                    </div>
-                  </li>
-                  <li>
-                    <div class="progressbar" style="width: 85%"></div>
-                    <div class="list-avatar">
-                      <div class="user-avatar">
-                        <img src="/images/cl-avatar2.png">
-                      </div>
-                    </div>
-                    <div class="list-title">
-                      <a href="#">
-                        <p>16 social postings on woman rights and movements around the world</p>
-                        <p class="small">15 DAYS AGO <span class="delimit">&middot;</span>  NEXT TASK: <strong>EDIT</strong></p>
-                      </a>
-                    </div>
-                    <div class="list-type">
-                      <i class="icon-type-facebook"></i>
-                    </div>
-                  </li>
-                </ul>
-
-
-              </div>
-
-
-
+              @include('campaign.partials.content_tab')
             </div>
-
 
             <!-- Tab2: Campaign Info -->
             <div role="tabpanel" class="tab-pane" id="contenttab-campaign">
@@ -190,7 +75,7 @@
 
                 <div class="input-form-group">
                   <label for="#">CAMPAIGN TITLE</label>
- {!! Form::text('campaign-title', @isset($campaign)? $campaign->title : '', array('placeholder' => 'Enter campaign title', 'class' => 'input input-larger form-control', 'id' => 'campaign-title')) !!}
+                  {!! Form::text('campaign-title', @isset($campaign)? $campaign->title : '', array('placeholder' => 'Enter campaign title', 'class' => 'input input-larger form-control', 'id' => 'campaign-title')) !!}
                 </div>
 
                 <div class="row">
