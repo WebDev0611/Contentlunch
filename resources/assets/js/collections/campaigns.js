95cba89081bf9c2f7a1f9446dd2e2a001217c98b
André Castelo <andrecastelo@gmail.com>
2017-02-10 19:33:25 -0200

    added fetch methods to the campaign collection

--- resources/assets/js/collections/campaigns.js
+++ resources/assets/js/collections/campaigns.js
@@ -1,4 +1,22 @@
-/* campaign collections */
+'use strict';
+
 var campaign_collection = Backbone.Collection.extend({
-	model: campaign_model
+	model: campaign_model,
+
+    populateList() {
+        return this.fetchData(campaignId).then(response => {
+            this.remove(this.models);
+            this.add(response.data.map(task => new task_model(task)));
+
+            return response;
+        });
+    },
+
+    fetchData() {
+        return $.ajax({
+            method: 'get',
+            url: `/api/campaigns`,
+            headers: getJsonHeader(),
+        });
+    },
 });
