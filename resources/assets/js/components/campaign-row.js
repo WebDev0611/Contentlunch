296df1381c062c126a39742b740e28351cbf721a
André Castelo <andrecastelo@gmail.com>
2017-02-13 22:50:40 -0200

    fixed campaign row format to look more like the blue stout design

--- resources/assets/js/components/campaign-row.js
+++ resources/assets/js/components/campaign-row.js
@@ -6,17 +6,33 @@ Vue.component('campaign-row', {
             <div class="dashboard-tasks-cell cell-size-5">
                 <img :src="campaign.profile_image" alt="#" class="dashboard-tasks-img">
             </div>
-            <div class="dashboard-tasks-cell cell-size-80">
+            <div class="dashboard-tasks-cell cell-size-55">
                 <h5 class="dashboard-tasks-title">
-                    {{ campaign.title }}
+                    <a :href="link">
+                        {{ campaign.title }}
+                    </a>
                 </h5>
                 <ul class="dashboard-tasks-list">
-                    <li>DUE ON: <strong>{{ campaign.due }}</strong></li>
+                    <li>UPDATED: <strong>{{ campaign.updated_at_diff.toUpperCase() }}</strong></li>
                 </ul>
             </div>
-            <div class="dashboard-tasks-cell">
-               SOMETHING
+            <div class="dashboard-tasks-cell cell-size-20">
+                <ul class="dashboard-tasks-list">
+                    <li>
+                        STARTED: <br />
+                        <strong>{{ campaign.started.toUpperCase() }}</strong>
+                    </li>
+                </ul>
             </div>
+            <div class="dashboard-tasks-cell cell-size-20">
+                <div class="dashboard-tasks-list">
+                    <li>
+                        ENDING: <br />
+                        <strong>{{ campaign.ending.toUpperCase() }}</strong>
+                    </li>
+                </div>
+            </div>
+            <div></div>
         </div>
     `,
 
@@ -24,5 +40,6 @@ Vue.component('campaign-row', {
 
     created() {
         this.campaign.profile_image = this.campaign.user.profile_image || '/images/cl-avatar2.png';
+        this.link = `/campaign/${this.campaign.id}`;
     }
 })
