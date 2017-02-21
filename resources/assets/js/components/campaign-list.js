02122db09e8fb6e39c09b736fb3724508d52f25d
André Castelo <andrecastelo@gmail.com>
2017-02-21 15:46:26 -0300

    added feedback to campaigns list when there are no campaigns

--- resources/assets/js/components/campaign-list.js
+++ resources/assets/js/components/campaign-list.js
@@ -4,6 +4,12 @@ Vue.component('campaign-list', {
     template: `
         <div>
             <loading v-if='!loaded'></loading>
+            <div class="dashboard-tasks-container" v-if="!campaigns.length && loaded">
+                <div class="dashboard-tasks-cell">
+                    <h5 class="dashboard-tasks-title">No campaigns: </h5>
+                    <a href="/campaign">create one now</a>
+                </div>
+            </div>
             <campaign-row v-for='campaign in campaigns' :campaign="campaign"></campaign-row>
         </div>
     `,
