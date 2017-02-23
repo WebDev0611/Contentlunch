a06e96f7312246e78d385e6015989ec86ecfd8cc
André Castelo <andrecastelo@gmail.com>
2017-02-23 14:41:36 -0300

    psr2

--- app/Http/Controllers/TrendsController.php
+++ app/Http/Controllers/TrendsController.php
@@ -3,11 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
-use App\Http\Requests;
-
 use GuzzleHttp\Client;
-
 use Illuminate\Support\Facades\Redis;
 
 class TrendsController extends Controller
@@ -19,7 +15,6 @@ class TrendsController extends Controller
      */
     public function index()
     {
-        //
     }
 
     /**
@@ -29,53 +24,52 @@ class TrendsController extends Controller
      */
     public function trending(Request $request)
     {
-
         $topic = $request->input('topic');
-        if(empty($topic)){
+        if (empty($topic)) {
             echo json_encode([]);
             exit;
         }
         $topic_key = 'trends:';
 
-        if(empty($topic)){
-            $topic_key .= "_";
-        }else{
-            $topic_key .= str_replace(' ', ',',$topic);
+        if (empty($topic)) {
+            $topic_key .= '_';
+        } else {
+            $topic_key .= str_replace(' ', ',', $topic);
         }
 
-        $topic_cache = Redis::get( $topic_key );
+        $topic_cache = Redis::get($topic_key);
         $output = '';
-       
-        function get_data($t){
-            $api_url = "https://api.rightrelevance.com/v2/articles/search?";
+
+        function get_data($t)
+        {
+            $api_url = 'https://api.rightrelevance.com/v2/articles/search?';
 
             $params = array(
-                "days" => "365",
-                "rgroup" => "large",
-                "query" => $t,
-                "access_token" => getenv('RIGHTRELEVANCE_TOKEN')
+                'days' => '365',
+                'rgroup' => 'large',
+                'query' => $t,
+                'access_token' => getenv('RIGHTRELEVANCE_TOKEN'),
             );
 
             $api_url .= http_build_query($params);
 
             $client = new Client();
-            $res = $client->request('GET', $api_url );
-  
-            $data_body =  $res->getBody();
+            $res = $client->request('GET', $api_url);
+
+            $data_body = $res->getBody();
+
             return json_decode($data_body->getContents());
         }
 
-        if( empty( unserialize($topic_cache) ) ){
-
+        if (empty(unserialize($topic_cache))) {
             $output = get_data($topic);
-            Redis::set($topic_key, serialize( $output ));
-            Redis::expire($topic_key, 60*20); //set cache for 10 min
-        }else{
+            Redis::set($topic_key, serialize($output));
+            Redis::expire($topic_key, 60 * 20); //set cache for 10 min
+        } else {
             $output = unserialize($topic_cache);
         }
 
-
-        return response()->json( $output );
+        return response()->json($output);
     }
 
     /**
@@ -85,62 +79,61 @@ class TrendsController extends Controller
      */
     public function create()
     {
-        //
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
+     *
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
+     *
      * @return \Illuminate\Http\Response
      */
     public function show($id)
     {
-        //
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
+     *
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
     {
-        //
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param \Illuminate\Http\Request $request
+     * @param int                      $id
+     *
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
     {
-        //
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param int $id
+     *
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
     {
-        //
     }
 }
