7c4d80668d29874677e24bc759c4ec2c5d499a36
André Castelo <andrecastelo@gmail.com>
2017-02-23 13:17:32 -0300

    added monthly scope to limit

--- app/Limit.php
+++ app/Limit.php
@@ -2,9 +2,18 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Limit extends Model
 {
     protected $guarded = [ 'id', 'created_at', 'updated_at' ];
+
+    public function scopeMonthly($query)
+    {
+        return $query->whereBetween('limit_user.created_at', [
+            Carbon::now()->subMonth(),
+            Carbon::now(),
+        ]);
+    }
 }
