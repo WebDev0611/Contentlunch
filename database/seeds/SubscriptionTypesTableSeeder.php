8ba04240d3d361387c6da96c7d9acf07881c27b9
André Castelo <andrecastelo@gmail.com>
2017-02-27 20:08:24 -0300

    changing the sub type seeder to handle custom limits

--- database/seeds/SubscriptionTypesTableSeeder.php
+++ database/seeds/SubscriptionTypesTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\SubscriptionType as Type;
 
 class SubscriptionTypesTableSeeder extends Seeder {
     /**
@@ -10,48 +11,46 @@ class SubscriptionTypesTableSeeder extends Seeder {
     public function run () {
         DB::statement('SET FOREIGN_KEY_CHECKS=0;');
 
-        DB::table('subscription_types')->truncate();
-        DB::table('subscription_types')->insert([
-            [
-                'name' => 'Trial',
-                'slug' => 'trial',
-                'price' => 0,
-                'price_per_client' => 0,
-                'limit_users' => 3,
-                'description' => 'Two week trial period.'
-            ],
-            [
-                'name' => 'Basic Monthly',
-                'slug' => 'basic-monthly',
-                'price' => 99.00,
-                'price_per_client' => 99.00,
-                'limit_users' => 5,
-                'description' => 'Basic plan restricts the number of users in the account to 5. All other functionalities are unlimited.'
-            ],
-            [
-                'name' => 'Basic Annually',
-                'slug' => 'basic-annually',
-                'price' => 1069.00,
-                'price_per_client' => 99.00,
-                'limit_users' => 5,
-                'description' => 'Basic plan restricts the number of users in the account to 5. All other functionalities are unlimited.'
-            ],
-            [
-                'name' => 'Pro Monthly',
-                'slug' => 'pro-monthly',
-                'price' => 199.00,
-                'price_per_client' => 99.00,
-                'limit_users' => 10,
-                'description' => 'Pro plan restricts the number of users in the account to 10. All other functionalities are unlimited.'
-            ],
-            [
-                'name' => 'Pro Annually',
-                'slug' => 'pro-annually',
-                'price' => 2149.00,
-                'price_per_client' => 99.00,
-                'limit_users' => 10,
-                'description' => 'Pro plan restricts the number of users in the account to 10. All other functionalities are unlimited.'
-            ]
+        Type::truncate();
+
+        $trial = Type::create([
+            'name' => 'Trial',
+            'price' => 0,
+            'price_per_client' => 0,
+            'limit_users' => 3,
+            'description' => 'Two week trial period.'
+        ]);
+
+        $basicMonthly = Type::create([
+            'name' => 'Basic Monthly',
+            'price' => 99.00,
+            'price_per_client' => 99.00,
+            'limit_users' => 5,
+            'description' => 'Basic plan restricts the number of users in the account to 5. All other functionalities are unlimited.'
+        ]);
+
+        $basicAnnually = Type::create([
+            'name' => 'Basic Annually',
+            'price' => 1069.00,
+            'price_per_client' => 99.00,
+            'limit_users' => 5,
+            'description' => 'Basic plan restricts the number of users in the account to 5. All other functionalities are unlimited.'
+        ]);
+
+        $proMonthly = Type::create([
+            'name' => 'Pro Monthly',
+            'price' => 199.00,
+            'price_per_client' => 99.00,
+            'limit_users' => 10,
+            'description' => 'Pro plan restricts the number of users in the account to 10. All other functionalities are unlimited.'
+        ]);
+
+        $proAnnually = Type::create([
+            'name' => 'Pro Annually',
+            'price' => 2149.00,
+            'price_per_client' => 99.00,
+            'limit_users' => 10,
+            'description' => 'Pro plan restricts the number of users in the account to 10. All other functionalities are unlimited.'
         ]);
 
         DB::statement('SET FOREIGN_KEY_CHECKS=1;');
