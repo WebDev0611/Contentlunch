77efc5589a12cf6c104316daa47e4a4422eba2eb
André Castelo <andrecastelo@gmail.com>
2017-02-27 19:31:19 -0300

    added fillable property

--- app/Subscription.php
+++ app/Subscription.php
@@ -4,12 +4,24 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Subscription extends Model {
-    public function subscriptionType () {
+class Subscription extends Model
+{
+    protected $fillable = [
+        'account_id',
+        'subscription_type_id',
+        'start_date',
+        'expiration_date',
+        'auto_renew',
+        'valid',
+    ];
+
+    public function subscriptionType()
+    {
         return $this->belongsTo('App\subscriptionType');
     }
 
-    public function account () {
+    public function account()
+    {
         return $this->belongsTo('App\Account');
     }
 }
