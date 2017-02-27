938dd28bf9d02b5758649741b0ab1fb44f2ea80f
André Castelo <andrecastelo@gmail.com>
2017-02-27 19:42:13 -0300

    subscriptionType factory

--- app/SubscriptionType.php
+++ app/SubscriptionType.php
@@ -6,6 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class SubscriptionType extends Model
 {
+    protected $fillable = [
+        'name',
+        'slug',
+        'price',
+        'price_per_client',
+        'limit_users',
+        'description',
+    ];
+
     public function subscriptions()
     {
         return $this->hasMany('App\Subscription');
---
+++ database/factories/SubscriptionTypeFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+$factory->define(App\SubscriptionType::class, function (Faker\Generator $faker) {
+    $name = ucwords($faker->word) . ' Plan';
+
+    return [
+        'name' => $name,
+        'slug' => \App\Helpers::slugify($name),
+        'price' => ($faker->numberBetween(0, 9) * 10) + 9.99,
+        'price_per_client' => ($faker->numberBetween(0, 9) * 10) + 9.99,
+        'limit_users' => $faker->numberBetween(5, 10),
+        'description' => $faker->sentence,
+    ];
+});
