e68dd7a46fab692bc8f2c0b0f8df101625cede58
Micah Potter <mpotter@nakedculture.co>
2017-03-01 00:05:17 -0500

    Add partial for google analytics and full story.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -139,19 +139,11 @@
     </script>
 @endif
 
-<script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-    ga('create', 'UA-92080489-1', 'auto');
-    ga('send', 'pageview');
-</script>
-
 @include('layouts.partials.js_user')
 @include('layouts.partials.intercom')
 @include('layouts.partials.freemium-popup')
+@include('layouts.partials.fullstory')
+@include('layouts.partials.google-analytics')
 
 </body>
 </html>
--- resources/views/layouts/minimal.blade.php
+++ resources/views/layouts/minimal.blade.php
@@ -21,20 +21,13 @@
 <script src="/js/vendor.js"></script>
 <script src="/js/plugins.js"></script>
 <script src="/js/app.js"></script>
-<script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-    ga('create', 'UA-92080489-1', 'auto');
-    ga('send', 'pageview');
-
-</script>
 @yield('scripts')
 
 @include('layouts.partials.js_user')
 @include('layouts.partials.intercom')
+@include('layouts.partials.fullstory')
+@include('layouts.partials.google-analytics')
 
 </body>
 </html>
