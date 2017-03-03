1bb75b244684c4a6c11ccedc85582b5dc973aae7
André Castelo <andrecastelo@gmail.com>
2017-03-03 16:29:41 -0300

    removed picto icons

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -23,12 +23,12 @@
                             <div class="input-form-group tight {{ $errors->has('email') ? 'has-error' : '' }}">
                                 <label for="#">Email Address</label>
                                 <div class="form-suffix form-suffix-login">
-                                    <i class="icon-email picto"></i>
+                                    {{--<i class="icon-email picto"></i>--}}
                                     <input  id="email"  name="email" type="email" class="input full" placeholder="Email address" value="{{ old('email') }}">
                                     @if ($errors->has('email'))
                                         <span class="help-block">
-                                                    <strong>{{ $errors->first('email') }}</strong>
-                                                </span>
+                                            <strong>{{ $errors->first('email') }}</strong>
+                                        </span>
                                     @endif
                                 </div>
                             </div>
@@ -36,14 +36,12 @@
                             <div class="input-form-group tight {{ $errors->has('password') ? 'has-error' : '' }}">
                                 <label for="#">Password</label>
                                 <div class="form-suffix form-suffix-login">
-                                    <i class="icon-password picto"></i>
-
+                                    {{--<i class="icon-password picto"></i>--}}
                                     <input id="password" type="password" class="input full" name="password">
-
                                     @if ($errors->has('password'))
                                         <span class="help-block">
-                                                <strong>{{ $errors->first('password') }}</strong>
-                                            </span>
+                                            <strong>{{ $errors->first('password') }}</strong>
+                                        </span>
                                     @endif
                                 </div>
                             </div>
