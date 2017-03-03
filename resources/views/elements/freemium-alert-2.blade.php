4310c45484b72142d42a764d51ba7ea736bafa0a
Ante Rejo <ante.rejo@gmail.com>
2017-03-03 14:37:12 +0100

    banner display fix

--- resources/views/elements/freemium-alert-2.blade.php
+++ resources/views/elements/freemium-alert-2.blade.php
@@ -1,7 +1,7 @@
+@if(App\Account::selectedAccount()->activeSubscriptions()->first()->subscriptionType->slug == 'trial')
+
 <div class="alert alert-info alert-forms freemium-notification-2 alert-dismissable">
     <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-
-    @if(App\Account::selectedAccount()->activeSubscriptions()->first()->subscriptionType->slug == 'trial')
 
         <div class="col-md-8 height-60">
             <h4>You are using the 2 week free trial of ContentLaunch</h4>
@@ -9,17 +9,6 @@
             <p>You can {!! $restriction !!} during this period. Switch to a paid account to remove
                 this and other limitations.</p>
         </div>
-
-    @elseif(App\Account::selectedAccount()->activeSubscriptions()->first()->subscriptionType->slug == 'free')
-
-        <div class="col-md-8 height-60">
-            <h4>You are using the free version of ContentLaunch</h4>
-
-            <p>You can {!! $restriction !!}. Switch to a paid account to remove
-                this and other limitations.</p>
-        </div>
-
-    @endif
 
     <div class="col-md-4 height-60">
         <a href="{{route('subscription')}}">
@@ -29,3 +18,26 @@
 
     <div class="clearfix"></div>
 </div>
+
+@elseif(App\Account::selectedAccount()->activeSubscriptions()->first()->subscriptionType->slug == 'free')
+
+    <div class="alert alert-info alert-forms freemium-notification-2 alert-dismissable">
+        <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+
+        <div class="col-md-8 height-60">
+            <h4>You are using the free version of ContentLaunch</h4>
+
+            <p>You can {!! $restriction !!}. Switch to a paid account to remove
+                this and other limitations.</p>
+        </div>
+
+        <div class="col-md-4 height-60">
+            <a href="{{route('subscription')}}">
+                <button class="btn btn-upgrade">Upgrade now</button>
+            </a>
+        </div>
+
+        <div class="clearfix"></div>
+    </div>
+
+@endif
--- resources/views/elements/freemium-alert.blade.php
+++ resources/views/elements/freemium-alert.blade.php
@@ -1,23 +1,31 @@
+@if(App\Account::selectedAccount()->activeSubscriptions()->first()->subscriptionType->slug == 'trial')
+
 <div class="alert alert-info alert-forms freemium-notification alert-dismissable">
     <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-
-    @if(App\Account::selectedAccount()->activeSubscriptions()->first()->subscriptionType->slug == 'trial')
 
         <h4>You are using the 2 week free trial of ContentLaunch</h4>
 
         <p>You can {!! $restriction !!} during this period. Switch to a paid account to remove
             this and other limitations.</p>
 
-    @elseif(App\Account::selectedAccount()->activeSubscriptions()->first()->subscriptionType->slug == 'free')
+    <a href="{{route('subscription')}}">
+        <button class="btn btn-upgrade">Upgrade now</button>
+    </a>
+</div>
+
+@elseif(App\Account::selectedAccount()->activeSubscriptions()->first()->subscriptionType->slug == 'free')
+
+    <div class="alert alert-info alert-forms freemium-notification alert-dismissable">
+        <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
 
         <h4>You are using the free version of ContentLaunch</h4>
 
         <p>You can {!! $restriction !!}. Switch to a paid account to remove
             this and other limitations.</p>
 
-    @endif
+        <a href="{{route('subscription')}}">
+            <button class="btn btn-upgrade">Upgrade now</button>
+        </a>
+    </div>
 
-    <a href="{{route('subscription')}}">
-        <button class="btn btn-upgrade">Upgrade now</button>
-    </a>
-</div>
+@endif
