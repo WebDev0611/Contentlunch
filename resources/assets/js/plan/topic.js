7cfb91ec364ca9808faa46bfc16369b333a0229c
André Castelo <andrecastelo@gmail.com>
2017-03-07 19:45:51 -0300

    moved camelize function to helpers

--- resources/assets/js/plan/topic.js
+++ resources/assets/js/plan/topic.js
@@ -1,13 +1,5 @@
 'use strict';
 
-var camelize = function(str) {
-    return str.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function(match, index) {
-        if (+match === 0) return ""; // or if (/\s+/.test(match)) for white spaces
-        return index == 0 ? match.toLowerCase() : match.toUpperCase();
-    });
-};
-
-
 (function($) {
 
 	var topic_result = Backbone.Model.extend({
