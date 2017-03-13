0f86f7d433a48f8cef0a63dd0e721aecb499b2fd
André Castelo <andrecastelo@gmail.com>
2017-03-13 19:14:48 -0300

    'can' is now an alias to our authorize middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -72,7 +72,7 @@ class Kernel extends HttpKernel
     protected $routeMiddleware = [
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
-        'can' => \Illuminate\Foundation\Http\Middleware\Authorize::class,
+        'can' => \App\Http\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'role' => \Zizaco\Entrust\Middleware\EntrustRole::class,
