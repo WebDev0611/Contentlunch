271fb8c37d5954d17bdcca76eaacff783c8b300b
André Castelo <andrecastelo@gmail.com>
2017-03-26 00:49:03 -0300

    added inviting user and account to the invite email

--- resources/views/emails/invite/email_invite.blade.php
+++ resources/views/emails/invite/email_invite.blade.php
@@ -4,15 +4,26 @@
 <meta charset="utf-8">
 </head>
 <body>
-	<h2>Content Launch Invite</h2>
-	<p>
-        Hey! <br>
-        I'm using Content Launch to manage our content marketing
-        efforts and I think you'll benefit from using it too. Check it out...
-        it's free, it's easy and it has lots of cool features we can use.
-        <br>
-        Click the link below (or copy and paste on your browser location bar) to
-        collaborate with me: <br><br>
+	<h1>Content Launch Invite</h1>
+    <p>Hey!</p>
+
+    <br>
+
+    <p>
+        {{ $user->name }} has invited you to collaborate on the {{ $account->name }} account! <br>
+        Content Launch is a tool to manage content marketing efforts and we think you'll benefit from using it too. <br>
+        Check it out... It's free, it's easy and it has lots of cool features you can use.
+    </p>
+
+    <br>
+
+    <p>
+        Click the link below (or copy and paste on your browser location bar) to collaborate with me:
+    </p>
+
+    <br>
+
+    <p>
         <a href="{{ $link }}">{{ $link }}</a>
     </p>
 </body>
