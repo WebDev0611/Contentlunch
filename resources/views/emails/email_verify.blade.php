525e7fcc870fea7591a870ae095778a55962507b
Ante Rejo <ante.rejo@gmail.com>
2017-04-03 20:18:21 +0200

    adding email styling

--- resources/views/emails/email_verify.blade.php
+++ resources/views/emails/email_verify.blade.php
@@ -1,20 +1,29 @@
 <!DOCTYPE html>
 <html lang="en-US">
 <head>
-    <meta charset="utf-8">
+    @include('emails.partials.head', ['title' => 'Verify Email Address Change'])
 </head>
 <body>
 <h2>Content Launch: Verify Email Address Change</h2>
 
-<div>
-    You recently requested a user email change from your Content Launch account. <br><br>
-    Please follow the link below to verify your new email address: <br>
-    <a href="{{ URL::to('settings/email/verify/' . $confirmation_code) }}">{{ URL::to('settings/email/verify/' . $confirmation_code) }}</a><br><br>
-    If you didn't make this request, please let us know immediately.<br><br>
-    Thanks,<br>
-    Content Launch Team
-
+<div class="content">
+    <div class="logo">
+        <img src="{{asset('images/logo-full.svg')}}" width="215" border="0" alt="" />
+    </div>
+    <h3>Verify Email Address Change</h3>
+    <p>
+        You recently requested a user email change from your Content Launch account. <br><br>
+        Please click the button below to verify your new email address: <br>
+        <a href="{{ URL::to('settings/email/verify/' . $confirmation_code) }}">
+            <button>Verify address change</button>
+        </a>
+        <br><br>
+        If you didn't make this request, please let us know immediately.<br><br>
+    </p>
+    <p class="thanks">
+        Thanks,<br>
+        Content Launch Team
+    </p>
 </div>
-
 </body>
 </html>
---
+++ resources/views/emails/partials/head.blade.php
@@ -0,0 +1,11 @@
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Content Launch: {{$title}}</title>
+<style type="text/css">
+    body {padding: 30px 0; min-width: 100%!important;font-family: sans-serif;text-align:center;}
+    .content {width: 100%; max-width: 600px;margin: 0 auto; padding: 10px; background-color:rgba(105,68,183,0.1);-webkit-border-radius: 6px;-moz-border-radius: 6px;border-radius: 6px;}
+    .logo{margin: 10px 0 30px;}
+    h3{margin: 0 auto 20px;}
+    button {text-align: center; font-weight: bold; padding: 0 30px 0 30px; margin-top:10px;}
+    button a {text-decoration: none;}
+    .thanks{text-align: left; padding-left: 20px;}
+</style>
