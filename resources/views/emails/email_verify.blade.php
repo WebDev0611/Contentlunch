ea1c9dd1aabc117c16fc1222a8cc333d35ed6331
Ante Rejo <ante.rejo@gmail.com>
2017-04-03 20:48:49 +0200

    email restyling

--- resources/views/emails/email_verify.blade.php
+++ resources/views/emails/email_verify.blade.php
@@ -11,14 +11,16 @@
         <img src="{{asset('images/logo-full.svg')}}" width="215" border="0" alt="" />
     </div>
     <h3>Verify Email Address Change</h3>
+
     <p>
-        You recently requested a user email change from your Content Launch account. <br><br>
-        Please click the button below to verify your new email address: <br>
-        <a href="{{ URL::to('settings/email/verify/' . $confirmation_code) }}">
-            <button>Verify address change</button>
-        </a>
-        <br><br>
-        If you didn't make this request, please let us know immediately.<br><br>
+        You recently requested a user email change from your Content Launch account. <br>
+        Please follow the link below to verify your new email address:
+    </p>
+    <a href="{{ URL::to('settings/email/verify/' . $confirmation_code) }}">
+        <button>Verify address change</button>
+    </a>
+    <p>
+        If you didn't make this request, please let us know immediately.
     </p>
     <p class="thanks">
         Thanks,<br>
--- resources/views/emails/partials/head.blade.php
+++ resources/views/emails/partials/head.blade.php
@@ -1,11 +1,12 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Content Launch: {{$title}}</title>
 <style type="text/css">
-    body {padding: 30px 0; min-width: 100%!important;font-family: sans-serif;text-align:center;}
-    .content {width: 100%; max-width: 600px;margin: 0 auto; padding: 10px; background-color:rgba(105,68,183,0.1);-webkit-border-radius: 6px;-moz-border-radius: 6px;border-radius: 6px;}
+    body {padding: 30px 0; min-width: 100%!important;-webkit-font-smoothing: antialiased;font-family: Tahoma, Geneva, sans-serif;text-align:center;background-color:#F2F2F2;color:#474747;}
+    .content {width: 100%; max-width: 600px;margin: 0 auto; padding: 20px; background-color:#fff;}
     .logo{margin: 10px 0 30px;}
-    h3{margin: 0 auto 20px;}
-    button {text-align: center; font-weight: bold; padding: 0 30px 0 30px; margin-top:10px;}
-    button a {text-decoration: none;}
+    h3{margin: 0 auto 20px;font-size:16px;}
+    p{font-size:14px;line-height: 24px;}
+    button {text-align: center; padding: 3px 30px; margin-top:10px; background-color: #2482ff; color:#fff; border: none;}
+    a button {cursor:pointer;}
     .thanks{text-align: left; padding-left: 20px;}
 </style>
