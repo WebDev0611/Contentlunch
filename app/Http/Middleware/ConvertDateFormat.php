d79555358a62095612c05c70f328c1346aa47648
André Castelo <andrecastelo@gmail.com>
2017-04-06 16:31:51 -0300

    added ConvertDatetimeFormat middleware and fixed a bug with the other one

--- app/Http/Middleware/ConvertDateFormat.php
+++ app/Http/Middleware/ConvertDateFormat.php
@@ -94,7 +94,7 @@ class ConvertDateFormat
      */
     protected function transform($key, $value)
     {
-        if ($key == $this->dateField && $value) {
+        if ($key === $this->dateField && $value) {
             return Carbon::createFromFormat($this->dateFormat, $value)->format('Y-m-d');
         }
 
