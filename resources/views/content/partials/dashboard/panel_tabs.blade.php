8973d72dd8994469ba3a4c0ce65639646aa381f4
André Castelo <andrecastelo@gmail.com>
2017-04-11 18:48:48 -0300

    added classes to panel tabs to target them with appcues

--- resources/views/content/partials/dashboard/panel_tabs.blade.php
+++ resources/views/content/partials/dashboard/panel_tabs.blade.php
@@ -1,15 +1,15 @@
 <div class="panel-header">
     <ul class="panel-tabs spacing">
-        <li class="{{ !Request::segment(2) ? 'active' : '' }}">
+        <li class="{{ !Request::segment(2) ? 'active' : '' }} panel-tabs-all-content">
             <a href="{{ route('contents.index') }}">All Content</a>
         </li>
-        <li class="{{ Request::segment(2) === 'orders' ? 'active' : '' }}">
+        <li class="{{ Request::segment(2) === 'orders' ? 'active' : '' }} panel-tabs-writing-orders">
             <a href="{{ route('content_orders.index') }}">Writing Orders in Process</a>
         </li>
-        <li class="{{ Request::segment(2) === 'campaigns' ? 'active' : '' }}">
+        <li class="{{ Request::segment(2) === 'campaigns' ? 'active' : '' }} panel-tabs-campaigns">
             <a href="{{ route('campaigns.index') }}">Campaigns</a>
         </li>
-        <li class="{{ Route::currentRouteName() === 'archived_contents.index' ? 'active' : '' }}">
+        <li class="{{ Route::currentRouteName() === 'archived_contents.index' ? 'active' : '' }} panel-tabs-archive">
             <a href="{{ route('archived_contents.index') }}">Archive</a>
         </li>
     </ul>
