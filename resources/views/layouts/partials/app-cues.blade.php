1083d452103f04f467c078edecab1018fd31b506
André Castelo <andrecastelo@gmail.com>
2017-04-10 21:56:04 -0300

    fixed bug with appcues config

--- resources/views/layouts/partials/app-cues.blade.php
+++ resources/views/layouts/partials/app-cues.blade.php
@@ -1,10 +1,10 @@
 <script src="//fast.appcues.com/14277.js"></script>
 @if (Auth::check())
 <script>
-    Appcues.identify("{{ $user->id }}", {
-        name: "{{ $user->name }}",
-        email: "{{ $user->email }}",
-        created_at: {{ $user->created_at->timestamp }},
+    Appcues.identify("{{ Auth::user()->id }}", {
+        name: "{{ Auth::user()->name }}",
+        email: "{{ Auth::user()->email }}",
+        created_at: {{ Auth::user()->created_at->timestamp }},
 
         belongs_to_agency_account: "{!! Auth::user()->belongsToAgencyAccount() !!}" === '1',
     });
