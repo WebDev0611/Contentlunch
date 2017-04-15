1834e82c899dbdf410125e7c5e2e74d6b3a05360
Micah Potter <mpotter@nakedculture.co>
2017-04-14 22:22:47 -0400

    Updated onboarding tests

--- test/Onboarding/RegisterTest.js
+++ test/Onboarding/RegisterTest.js
@@ -2,7 +2,7 @@ module.exports = {
     'Register Account': function (browser) {
         browser
             // Check login page and click register
-            .url('https://test.contentlaunch.com')
+            .url('http://localhost:8888')
             .waitForElementVisible('form[action$=login]', 1000, "Login form is visible")
             .click('a[href$=signup]')
 
@@ -15,6 +15,7 @@ module.exports = {
             .setValue('#company_name', 'ContentLaunch')
             .setValue('[name=account_type]', '1')
             .click('input[type=submit]')
+            .pause(50000)
 
             // Check invite page
             .assert.urlContains("/invite", "Account created successfully")
@@ -30,10 +31,15 @@ module.exports = {
             // Check score page
             .assert.urlContains("/score", "Navigated to Score page successfully")
             .waitForElementVisible(".onboarding-step-point.active:nth-of-type(4)", 1000, "Progress meter shows complete")
-            .click('a')
+            .click('#goToAppLink')
 
             // Check Dashboard
-            .assert.urlContains("/", "Navigated to Dashboard successfully");
+            .waitForElementVisible('.navigation', 1000, 'Navigation panel is visible.')
+            .waitForElementVisible('.icon-navigation-chart', 1000, 'Dashboard icon is visible.')
+            .waitForElementVisible('.icon-navigation-content', 1000, 'Plan icon is visible.')
+            .waitForElementVisible('.icon-navigation-dashboard', 1000, 'Create is visible.')
+            .waitForElementVisible('.icon-navigation-calendar', 1000, 'Calendar is visible.')
+            //.assert.urlEquals("/", "Navigated to Dashboard successfully");
 
         browser.end();
     }
