7a3948195ba611a406a6ff7b27e1d1adc3fd6180
André Castelo <andrecastelo@gmail.com>
2017-04-17 22:29:37 -0300

    added presenter + relationship with users

--- app/Login.php
+++ app/Login.php
@@ -2,13 +2,30 @@
 
 namespace App;
 
+use App\Presenters\LoginPresenter;
+use App\Traits\Orderable;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
+use Laracasts\Presenter\PresentableTrait;
 
 class Login extends Model
 {
+    use Orderable, PresentableTrait;
+
+    protected $presenter = LoginPresenter::class;
+
     protected $fillable = ['user_id', 'ip_address', 'user_agent', 'fingerprint'];
+
+    /*
+     |--------------------------------------------------------------------------
+     | Relationships
+     |--------------------------------------------------------------------------
+     */
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 
     public static function recentLoginsCount($daysAgo = 7)
     {
