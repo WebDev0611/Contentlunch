5b09c2ab539078c36564d3ff9bb54edc1107b92c
André Castelo <andrecastelo@gmail.com>
2017-04-17 22:29:25 -0300

    added LoginPresenter

---
+++ app/Presenters/LoginPresenter.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Presenters;
+
+use App\Presenters\Helpers\BasePresenter;
+use App\Presenters\Helpers\CreatedAtPresenter;
+
+class LoginPresenter extends BasePresenter
+{
+    use CreatedAtPresenter;
+}
