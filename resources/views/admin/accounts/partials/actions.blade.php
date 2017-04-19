28b1a7430425751d7f8a26dc35cd69d2b28c0780
André Castelo <andrecastelo@gmail.com>
2017-04-19 13:37:18 -0300

    save button will only show on edit and create pages

--- resources/views/admin/accounts/partials/actions.blade.php
+++ resources/views/admin/accounts/partials/actions.blade.php
@@ -1,4 +1,13 @@
 <div class="m-b">
+    @php
+        $route = Route::getCurrentRoute()->getName();
+    @endphp
+    @if ($route === 'admin.accounts.edit')
+    <a href="{{ route('admin.accounts.update', $account) }}" class="btn btn-primary">
+        <i class="fa fa-save"></i>
+        Save Account
+    </a>
+    @endif
     <a href="{{ route('admin.accounts.edit', $account) }}" class="btn btn-white">
         <i class="fa fa-pencil"></i>
         Edit Account
