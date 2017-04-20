12b080ef7979c8f87c7f6c4a1c9671ed5bbb6874
Ante Rejo <ante.rejo@gmail.com>
2017-04-20 17:25:25 +0200

    modifying mailchimp wrapper to work with oAuth

--- app/libraries/Connections/API/MailchimpAPI.php
+++ app/libraries/Connections/API/MailchimpAPI.php
@@ -2,46 +2,19 @@
 
 namespace Connections\API;
 
-
-use DrewM\MailChimp\MailChimp;
+use App\MailChimp;
+use oAuth\API\MailchimpAuth;
 
 class MailchimpAPI {
 
+    public function __construct ($content = null, $connection = null) {
+        $this->client = null;
+        $this->content = $content;
+        $this->connection = $connection ? $connection : $this->content->connection;
+    }
+    
     public function createCampaign ()
     {
-        $MailChimp = new MailChimp('3f750b946b6d8dcece0fad5e3f94794f-us15');
 
-        $campaignData = [
-            'type'             => 'regular', // accepted values: regular, plaintext, absplit, rss, variate
-            'recipients'       => [
-                'list_id' => '0c0fcaa3fe'   // string, TODO: GET 'lists' and create dropdown
-            ],
-            'settings'         => [
-                'subject_line' => 'This is a subject 3',
-                'title'        => 'This is a title 3',
-                'from_name'    => 'Content Launch',
-                'reply_to'     => 'testing@contentlaunch.app'
-            ],
-            'variate_settings' => [
-                'winner_criteria' => 'opens'    // opens clicks manual total_revenue
-            ],
-            'rss_opts'         => [
-                'feed_url'  => 'http://contentlaunch.app',
-                'frequency' => 'daily'  // Possible Values: daily weekly monthly
-            ]
-        ];
-
-        //$result = $MailChimp->post('campaigns', $campaignData);
-        //$result = $MailChimp->get('lists');
-        //$result = $MailChimp->get('campaigns');
-
-        $result = $MailChimp->put('campaigns/9887044d82/content', [
-            'html' => '<h1>Lorem Ipsum 1234</h1><p>Abcd Efg Hijk</p>',
-            //        'template' => [
-            //            'id' => 12     // int, TODO: GET 'templates' and create dropdown
-            //        ]
-        ]);
-
-        var_dump($result);
     }
 }
