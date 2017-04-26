0577ea9c7595d3c9f99a25b5a357fd8bbcb73591
André Castelo <andrecastelo@gmail.com>
2017-04-26 19:30:32 -0300

    implementing messaging system interface

---
+++ resources/assets/js/components/messaging-system/MessagesTeamMember.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="messages-list-team-member">
+        <div class="messages-list-cell">
+            <div class="dashboard-tasks-img-wrapper">
+                <img :src="user.profile_image" alt="#" class="dashboard-tasks-img">
+            </div>
+        </div>
+        <div class="messages-list-cell">
+            <p class="dashboard-ideas-text">
+                {{ user.name }} <span v-show='messages' class="badge">{{ messages }}</span>
+            </p>
+            <small>
+                {{ user.email }}
+            </small>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'messages-team-member',
+
+        props: [ 'user', 'messages' ],
+    }
+</script>
