4dca190df474b2dd9185e0618358e5424e69d718
André Castelo <andrecastelo@gmail.com>
2017-05-04 14:15:11 -0300

    test now checks if the influencer was saved correctly in the db

--- tests/controllers/InfluencerControllerTest.php
+++ tests/controllers/InfluencerControllerTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Account;
+use App\Influencer;
 use App\User;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
@@ -36,7 +37,7 @@ class InfluencerControllerTest extends TestCase
             'image_url' => 'http://pbs.twimg.com/profile_images/745781059040645120/Hg8W0kXe_normal.jpg',
         ];
 
-        $this->json('POST', route('influencers.toggle_bookmark'), $data)
+        $request = $this->json('POST', route('influencers.toggle_bookmark'), $data)
             ->assertResponseStatus(201)
             ->seeJson([ 'twitter_followers_count' => 47639 ])
             ->seeJson([ "twitter_id_str" => "28870687" ])
@@ -54,5 +55,16 @@ class InfluencerControllerTest extends TestCase
                     'image_url',
                 ]
             ]);
+
+        $influencer = $request->response->getData()->data;
+        $influencerDB = $this->account->influencers()->find($influencer->id);
+
+        $this->assertEquals(1, $this->account->influencers()->count());
+        $this->assertEquals($influencer->twitter_followers_count, $influencerDB->twitter_followers_count);
+        $this->assertEquals($influencer->twitter_id_str, $influencerDB->twitter_id_str);
+        $this->assertEquals($influencer->twitter_screen_name, $influencerDB->twitter_screen_name);
+        $this->assertEquals($influencer->name, $influencerDB->name);
+        $this->assertEquals($influencer->description, $influencerDB->description);
+        $this->assertEquals($influencer->image_url, $influencerDB->image_url);
     }
 }
