9579b8d538b4960e11ddef9b657e7f557a06c6a4
Ante Rejo <ante.rejo@gmail.com>
2017-05-05 20:10:36 +0200

    controller refactor

--- app/Http/Controllers/Connections/GoogleController.php
+++ app/Http/Controllers/Connections/GoogleController.php
@@ -2,14 +2,13 @@
 
 namespace App\Http\Controllers\Connections;
 
-
 use Illuminate\Http\Request;
 use oAuth\API\GoogleAnalyticsAuth;
 use oAuth\API\GoogleDriveAuth;
 
 class GoogleController extends BaseConnectionController {
 
-    protected $service;
+    protected $auth;
 
     public function callback (Request $request, $service)
     {
@@ -19,26 +18,27 @@ class GoogleController extends BaseConnectionController {
             return $this->redirectWithError(json_encode($request->input('error')));
         }
 
-        if(!$request->has('code')) {
+        if (!$request->has('code')) {
             return $this->redirectWithError('There was an error with your authentication, please try again');
         }
 
-        $this->service = $service;
+        switch ($service) {
+            case 'drive':
+                $providerSlug = 'google-drive';
+                $this->auth = new GoogleDriveAuth();
+                break;
+            case 'analytics':
+                $providerSlug = 'google-analytics';
+                $this->auth = new GoogleAnalyticsAuth();
+                break;
+            default:
+                return $this->redirectWithError('There was an error with callback, please try again');
+        }
+
         $token = $this->codeForToken($request->input('code'));
 
         if (collect($token)->has('error')) {
             return $this->redirectWithError('There was an error with your authentication, please try again');
-        }
-
-        switch ($this->service) {
-            case 'drive':
-                $providerSlug = 'google-drive';
-                break;
-            case 'analytics':
-                $providerSlug = 'google-analytics';
-                break;
-            default:
-                return $this->redirectWithError('There was an error with callback, please try again');
         }
 
         $connection = $this->saveConnection($token, $providerSlug);
@@ -46,32 +46,10 @@ class GoogleController extends BaseConnectionController {
         return $this->redirectWithSuccess("You've successfully connected to Google.");
     }
 
-    public function codeForToken($code)
+    public function codeForToken ($code)
     {
-        switch ($this->service) {
-            case 'drive':
-                $auth = new GoogleDriveAuth();
-                break;
-            case 'analytics':
-                $auth = new GoogleAnalyticsAuth();
-                break;
-        }
+        $this->auth->client->authenticate($code);
 
-        $auth->client->authenticate($code);
-
-        return $auth->client->getAccessToken();
-    }
-
-    public function tokenPostData($code)
-    {
-        $auth = new GoogleDriveAuth();
-
-        return [
-            'client_id' => $auth->client->getClientId(),
-            'redirect_uri' =>  $auth->client->getRedirectUri(),
-            'client_secret' =>  $auth->client->getClientSecret(),
-            'code' => $code,
-            'grant_type' => 'authorization_code'
-        ];
+        return $this->auth->client->getAccessToken();
     }
 }
