8355baec8891093aae2d462180cfb79a04a51e2b
André Castelo <andrecastelo@gmail.com>
2017-05-23 17:06:59 -0300

    moved code that changed contents' status to its own trait

--- app/Content.php
+++ app/Content.php
@@ -3,6 +3,7 @@
 use App\Account;
 use App\Helpers;
 use App\Presenters\ContentPresenter;
+use App\Traits\ConfiguresContentActions;
 use App\Traits\Orderable;
 use Auth;
 use Carbon\Carbon;
@@ -13,7 +14,7 @@ use Spatie\Activitylog\Traits\LogsActivity;
 
 class Content extends Model
 {
-    use PresentableTrait, Orderable, LogsActivity;
+    use PresentableTrait, Orderable, LogsActivity, ConfiguresContentActions;
 
     public $presenter = ContentPresenter::class;
 
@@ -177,69 +178,6 @@ class Content extends Model
     public function tasks()
     {
         return $this->belongsToMany('App\Task');
-    }
-
-    /**
-     * Configures the flags ready_published, written and published according to the
-     * $action string
-     */
-    public function configureAction($action = null)
-    {
-        $status = ContentStatus::BEING_WRITTEN;
-
-        switch ($action) {
-            case 'ready_to_publish': $status = ContentStatus::READY; break;
-            case 'publish': $status = ContentStatus::PUBLISHED; break;
-            case 'archived': $status = ContentStatus::ARCHIVED; break;
-            default: break;
-        }
-
-        $this->status()->associate($status)->save();
-    }
-
-    public function scopeReadyToPublish($query)
-    {
-        return $query->where('content_status_id', ContentStatus::READY);
-    }
-
-    public function scopeWritten($query)
-    {
-        return $query->where('content_status_id', ContentStatus::BEING_WRITTEN);
-    }
-
-    public function scopePublished($query)
-    {
-        return $query->where('content_status_id', ContentStatus::PUBLISHED);
-    }
-
-    public function scopeArchived($query)
-    {
-        return $query->where('content_status_id', ContentStatus::ARCHIVED);
-    }
-
-    public function scopeCurrent ($query)
-    {
-        return $query->whereIn('content_status_id', [ContentStatus::READY, ContentStatus::BEING_WRITTEN]);
-    }
-
-    public function setReadyPublished()
-    {
-        $this->configureAction('ready_to_publish');
-    }
-
-    public function setWritten()
-    {
-        $this->configureAction('written_content');
-    }
-
-    public function setPublished()
-    {
-        $this->configureAction('publish');
-    }
-
-    public function setArchived()
-    {
-        $this->configureAction('archived');
     }
 
     /**
