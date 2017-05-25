2058eb94c4dbc2b5654f844006d3d9690190b860
André Castelo <andrecastelo@gmail.com>
2017-05-25 00:37:25 -0300

    added presenter to activity model

--- app/Activity.php
+++ app/Activity.php
@@ -2,9 +2,13 @@
 
 namespace App;
 
+use App\Presenters\ActivityPresenter;
 use Illuminate\Database\Eloquent\Model;
+use Laracasts\Presenter\PresentableTrait;
 
 class Activity extends \Spatie\Activitylog\Models\Activity
 {
-    //
+    use PresentableTrait;
+
+    protected $presenter = ActivityPresenter::class;
 }
