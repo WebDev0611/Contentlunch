81823ec6abb28b7a70710cbdf10d972c43abe1ee
Ante Rejo <ante.rejo@gmail.com>
2017-05-31 23:50:16 +0200

    changing writeraccess email templates

--- resources/views/emails/writeraccess_pending_order.blade.php
+++ resources/views/emails/writeraccess_pending_order.blade.php
@@ -5,7 +5,7 @@
 @extends('emails.layouts.master', $staticData)
 @section('content')
     <p>
-        This is an automatic email notification from Content Launch triggered by WriterAccess.<br><br>
+        This is an automatic email notification from the Content Launch app.<br><br>
         <b>You have Content Orders that are pending approval.</b>
     </p>
     <a href="{{route('content_orders.index')}}">
