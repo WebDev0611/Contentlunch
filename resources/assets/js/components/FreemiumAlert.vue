94a837ba9b3181ff8067aa36495931f02a109ff9
André Castelo <andrecastelo@gmail.com>
2017-06-03 23:19:39 -0300

    added freemium alert component

---
+++ resources/assets/js/components/FreemiumAlert.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="alert alert-info alert-forms freemium-notification alert-dismissable">
+
+        <h4>You are using the {{ accountPlan }} of ContentLaunch</h4>
+
+        <p>
+            You can {{ restrictionMessage }} during this period.
+            Switch to a paid account to remove this and other limitations.
+        </p>
+
+        <a href="/subscription">
+            <button class="btn btn-upgrade">Upgrade now</button>
+        </a>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'freemium-alert',
+
+        props: ['type', 'restriction'],
+
+        computed: {
+            restrictionMessage() {
+                return this.restriction || 'try out all features of the app, but some functionality is limited';
+            },
+
+            accountPlan() {
+                switch (this.type) {
+                    case 'trial': return '2 week free trial';
+                    case 'free': return 'free version';
+                    default: return null;
+                }
+            }
+        }
+    }
+</script>
