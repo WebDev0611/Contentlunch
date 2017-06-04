1b8406301e3c6edc879e7681974a892e6969a288
André Castelo <andrecastelo@gmail.com>
2017-06-03 22:48:29 -0300

    added avatar component

---
+++ resources/assets/js/components/Avatar.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="dashboard-tasks-img-wrapper">
+        <img :src="profileImage" alt="#" class="dashboard-tasks-img">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'avatar',
+
+        props: [ 'user' ],
+
+        computed: {
+            profileImage() {
+                return this.user.profile_image || '/images/cl-avatar2.png';
+            },
+        }
+    }
+</script>
