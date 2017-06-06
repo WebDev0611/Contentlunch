d34a30134229813fa79e57cff282a3f73e046709
André Castelo <andrecastelo@gmail.com>
2017-06-05 23:28:05 -0300

    added code to invite users from the invite form component

--- resources/assets/js/components/InviteForm.vue
+++ resources/assets/js/components/InviteForm.vue
@@ -7,9 +7,10 @@
         <div class="alert alert-danger alert-forms" v-show='showAlert'>
             Please enter one or more email addresses.
         </div>
-        <button class="send-invitation button button-extend text-uppercase" @click='inviteUsers'>
+        <button class="send-invitation button button-extend text-uppercase" @click='inviteUsers' v-show='!loading'>
             Send Invitation
         </button>
+        <loading v-show='loading'></loading>
     </div>
 </template>
 
@@ -21,18 +22,57 @@
             return {
                 emails: '',
                 showAlert: false,
+                loading: false,
             };
         },
 
         methods: {
             inviteUsers() {
-                if (emails === '') {
+                if (this.emails === '') {
                     this.showAlert = true;
                     return false;
                 }
 
+                let payload = { emails: this.emails };
+
+                this.loading = true;
+
+                $.post('/invite/emails', payload)
+                    .then(this.handleSuccess.bind(this))
+                    .catch(this.handleError.bind(this));
+            },
+
+            handleSuccess(response) {
+                let emailsCount = this.emails.split(',').length;
+
                 this.$emit('invited');
-            }
+                this.emails = '';
+                this.loading = false;
+                this.showSuccessFeedback(emailsCount);
+            },
+
+            handleError(response) {
+                this.loading = false;
+                this.$emit('invited');
+
+                if (response.status === 403) {
+                    showUpgradeAlert(response.responseJSON.data);
+                } else {
+                    swal('Error!', response.responseJSON.data, 'error')
+                }
+            },
+
+            showSuccessFeedback(count) {
+                let title = 'Invites Sent!';
+                let message = 'The invites were sent successfully.';
+
+                if (count === 1) {
+                    title = 'Invite Sent!';
+                    message = 'The invite was sent successfully.';
+                }
+
+                swal(title, message, 'success');
+            },
         },
     }
 </script>
