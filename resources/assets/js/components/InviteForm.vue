1ef065727bef37939bfe4ad8970debabfb7762cb
André Castelo <andrecastelo@gmail.com>
2017-06-05 23:15:15 -0300

    added invite form component

---
+++ resources/assets/js/components/InviteForm.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="inner">
+        <div class="input-form-group">
+            <label>Invite</label>
+            <input type="text" v-model='emails' class="email-invites input" placeholder="One or more e-mail addresses separated by commas">
+        </div>
+        <div class="alert alert-danger alert-forms" v-show='showAlert'>
+            Please enter one or more email addresses.
+        </div>
+        <button class="send-invitation button button-extend text-uppercase" @click='inviteUsers'>
+            Send Invitation
+        </button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'invite-form',
+
+        data() {
+            return {
+                emails: '',
+                showAlert: false,
+            };
+        },
+
+        methods: {
+            inviteUsers() {
+                if (emails === '') {
+                    this.showAlert = true;
+                    return false;
+                }
+
+                this.$emit('invited');
+            }
+        },
+    }
+</script>
