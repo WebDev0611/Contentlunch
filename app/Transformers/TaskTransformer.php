3ef6fa7cd56347eb918032fc01245d4ef578f184
André Castelo <andrecastelo@gmail.com>
2017-06-07 16:00:35 -0300

    implemented fractal service provider

---
+++ app/Transformers/TaskTransformer.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Transformers;
+
+use App\Task;
+use League\Fractal\TransformerAbstract;
+
+class TaskTransformer extends TransformerAbstract
+{
+    protected $availableIncludes = [
+        'user',
+    ];
+
+    public function transform(Task $task)
+    {
+        return [
+            'id' => (int) $task->id,
+            'name' => $task->name,
+            'explanation' => $task->explanation,
+            'start_date' => $task->start_date,
+            'due_date' => $task->due_date,
+            'user_id' => $task->user_id,
+            'account_id' => $task->account_id,
+            'url' => $task->url,
+            'created_at' => (string) $task->created_at,
+            'updated_at' => (string) $task->updated_at,
+            'status' => $task->status,
+            'due_date_diff' => $task->present()->dueDate,
+            'updated_at_diff' => $task->present()->updatedAt,
+            'created_at_diff' => $task->present()->createdAt,
+        ];
+    }
+
+    public function includeUser(Task $task)
+    {
+        return $this->item($task->user, new UserTransformer);
+    }
+}
