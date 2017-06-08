b20dd6afe9dd9ba27d12342b2e36f175a208f2bd
Ante Rejo <ante.rejo@gmail.com>
2017-06-08 20:46:49 +0200

    adding migrations

---
+++ database/migrations/2017_06_08_140210_create_writer_access_writers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWriterAccessWritersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('writer_access_writers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('writer_id')->unsigned()->unique();
+            $table->string('name');
+            $table->string('location')->nullable();
+            $table->float('rating');
+            $table->string('photo')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('writer_access_writers');
+    }
+}
