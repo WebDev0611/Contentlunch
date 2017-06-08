f3d5d4d5aa70c4c1fbf64aa2788bed8a285ca10e
André Castelo <andrecastelo@gmail.com>
2017-06-08 17:00:52 -0300

    addded idea transformer

---
+++ app/Transformers/IdeaTransformer.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Transformers;
+
+use League\Fractal\TransformerAbstract;
+
+class IdeaTransformer extends TransformerAbstract
+{
+    protected $availableInclues = [
+        'user',
+    ];
+
+    public function transform(Idea $idea)
+    {
+        return [
+            'id' => $idea->id,
+            'user_id' => $idea->user_id,
+            'account_id' => $idea->account_id,
+            'name' => $idea->name,
+            'text' => $idea->text,
+            'tags' => $idea->tags,
+            'created_at' => (string) $idea->created_at,
+            'updated_at' => (string) $idea->updated_at,
+            'created_diff' => $idea->created_at->diffForHumans(),
+            'updated_diff' => $idea->updated_at->diffForHumans(),
+            'status' => $idea->status,
+            'calendar_id' => $idea->calendar_id,
+        ];
+    }
+
+    public function includeUser(Idea $idea)
+    {
+        return $this->item($idea->user, new UserTransformer);
+    }
+}
