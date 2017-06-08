289630fcc13aada29b295d224b9254003139150f
André Castelo <andrecastelo@gmail.com>
2017-06-08 00:54:05 -0300

    changed wording - we're not going to load all tasks, just load more

--- resources/assets/js/components/LoadMoreButton.vue
+++ resources/assets/js/components/LoadMoreButton.vue
@@ -2,7 +2,7 @@
     <div class="create-panel-table" v-show='totalLeft && totalLeft > 0'>
         <div class="create-panel-table-cell text-center">
             <a style='cursor:pointer'>
-                {{ totalLeft }} More - Show All
+                {{ totalLeft }} left - Show More
             </a>
         </div>
     </div>
