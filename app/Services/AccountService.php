e288932b11c0a8516bd8e9c30a4610fd12f72906
André Castelo <andrecastelo@gmail.com>
2017-06-09 15:20:01 -0300

    simplified account service activity method

--- app/Services/AccountService.php
+++ app/Services/AccountService.php
@@ -40,56 +40,9 @@ class AccountService
 
     public function activityFeed()
     {
-        return $this->contentActivity()
-            ->merge($this->taskActivity())
-            ->merge($this->campaignActivity())
-            ->sort(function($a, $b) {
-                return $a->created_at->lt($b->created_at) ? 1 : -1;
-            })
-            ->map(function($activity) {
-                return [
-                    'id' => $activity->id,
-                    'user' => $activity->present()->causer,
-                    'action' => $activity->present()->action,
-                    'subject_type' => $activity->present()->subjectType,
-                    'subject_name' => $activity->present()->subjectName,
-                    'subject_link' => $activity->present()->subjectLink,
-                    'date' => (string) $activity->present()->createdAtFormat('m/d/Y H:i:s'),
-                ];
-            })
-            ->values();
-    }
-
-    protected function activityQuery()
-    {
-        return Activity::with('subject')
-            ->select('activity_log.*')
+        return Activity::select('activity_log.*')
+            ->with('subject')
             ->orderBy('activity_log.created_at', 'desc')
-            ->where('account_id', $this->selectedAccount->id)
-            ->take(10);
-    }
-
-    public function contentActivity()
-    {
-        return $this->activityQuery()
-            ->join('contents', 'contents.id', '=', 'activity_log.subject_id')
-            ->where('activity_log.subject_type', 'App\Content')
-            ->get();
-    }
-
-    public function taskActivity()
-    {
-        return $this->activityQuery()
-            ->join('tasks', 'tasks.id', '=', 'activity_log.subject_id')
-            ->where('activity_log.subject_type', 'App\Task')
-            ->get();
-    }
-
-    public function campaignActivity()
-    {
-        return $this->activityQuery()
-            ->join('campaigns', 'campaigns.id', '=', 'activity_log.subject_id')
-            ->where('activity_log.subject_type', 'App\Campaign')
-            ->get();
+            ->where('activity_log.account_id', $this->selectedAccount->id);
     }
 }
