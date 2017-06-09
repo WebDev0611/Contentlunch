ac28e992b043fba503feb4f246b1c11761c48574
André Castelo <andrecastelo@gmail.com>
2017-06-09 15:33:02 -0300

    fixed a bug where the campaign transformer wasn't working properly

--- app/Transformers/CampaignTransformer.php
+++ app/Transformers/CampaignTransformer.php
@@ -2,6 +2,7 @@
 
 namespace App\Transformers;
 
+use App\Campaign;
 use League\Fractal\TransformerAbstract;
 
 class CampaignTransformer extends TransformerAbstract
@@ -10,15 +11,15 @@ class CampaignTransformer extends TransformerAbstract
     {
         return [
             'title' => $campaign->present()->title,
-            'user_id', => $campaign->user_id,
-            'account_id', => $campaign->account_id,
-            'status', => $campaign->status,
-            'campaign_type_id', => $campaign->campaign_type_id,
-            'start_date', => (string) $campaign->start_date,
-            'end_date', => (string) $campaign->end_date,
-            'is_recurring', => $campaign->is_recurring,
-            'description', => $campaign->description,
-            'goals', => $campaign->goals,
+            'user_id' => $campaign->user_id,
+            'account_id' => $campaign->account_id,
+            'status' => $campaign->status,
+            'campaign_type_id' => $campaign->campaign_type_id,
+            'start_date' => (string) $campaign->start_date,
+            'end_date' => (string) $campaign->end_date,
+            'is_recurring' => $campaign->is_recurring,
+            'description' => $campaign->description,
+            'goals' => $campaign->goals,
             'interval' => $campaign->interval,
             'created_at' => (string) $campaign->created_at,
             'updated_at' => (string) $campaign->updated_at,
