5c1e35fe61961c21550e11f09c78f693684b4789
André Castelo <andrecastelo@gmail.com>
2017-06-09 10:23:57 -0300

    altered the recent content list item to use the avatar component

--- resources/assets/js/components/dashboard/RecentContentListItem.vue
+++ resources/assets/js/components/dashboard/RecentContentListItem.vue
@@ -2,7 +2,7 @@
     <div>
         <div class="dashboard-ideas-cell cell-size-5">
             <div class="dashboard-tasks-img-wrapper">
-                <img :src="imageProfile" alt="#" class="dashboard-tasks-img">
+                <avatar :user='content.user'></avatar>
             </div>
         </div>
         <div class="dashboard-members-cell cell-size-75">
@@ -33,14 +33,6 @@
         props: [ 'content' ],
 
         computed: {
-            imageProfile() {
-                let defaultAvatar = '/images/cl-avatar2.png';
-
-                return this.content.author
-                    ? this.content.author.profile_image || defaultAvatar
-                    : defaultAvatar;
-            },
-
             contentStage() {
                 return this.content.content_status_id;
             },
