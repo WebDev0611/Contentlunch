2f7cc6dd5d686808ef2398e4777ad74a0d053d0b
André Castelo <andrecastelo@gmail.com>
2017-06-09 15:19:16 -0300

    added campaign tranformer

---
+++ app/Transformers/CampaignTransformer.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Transformers;
+
+use League\Fractal\TransformerAbstract;
+
+class CampaignTransformer extends TransformerAbstract
+{
+    public function transform(Campaign $campaign)
+    {
+        return [
+            'title' => $campaign->present()->title,
+            'user_id', => $campaign->user_id,
+            'account_id', => $campaign->account_id,
+            'status', => $campaign->status,
+            'campaign_type_id', => $campaign->campaign_type_id,
+            'start_date', => (string) $campaign->start_date,
+            'end_date', => (string) $campaign->end_date,
+            'is_recurring', => $campaign->is_recurring,
+            'description', => $campaign->description,
+            'goals', => $campaign->goals,
+            'interval' => $campaign->interval,
+            'created_at' => (string) $campaign->created_at,
+            'updated_at' => (string) $campaign->updated_at,
+        ];
+    }
+}
