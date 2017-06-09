f66cc93ab514da16b715fff2ef38fcf382dc8044
André Castelo <andrecastelo@gmail.com>
2017-06-09 13:43:22 -0300

    added activity transformer

---
+++ app/Transformers/ActivityTransformer.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Transformers;
+
+use App\Activity;
+use League\Fractal\TransformerAbstract;
+
+class ActivityTransformer extends TransformerAbstract
+{
+    protected $defaultIncludes = [
+        'user',
+        'subject',
+    ];
+
+    public function transform(Activity $activity)
+    {
+        return [
+            'id' => $activity->id,
+            'user' => $activity->present()->causer,
+            'action' => $activity->present()->action,
+            'subject_type' => $activity->present()->subjectType,
+            'subject_name' => $activity->present()->subjectName,
+            'subject_link' => $activity->present()->subjectLink,
+            'date' => (string) $activity->present()->createdAtFormat('m/d/Y H:i:s'),
+        ];
+    }
+
+    public function includeUser(Activity $activity)
+    {
+        return $activity->user ? $this->item($activity->user, new UserTransformer) : null;
+    }
+
+    public function includeSubject(Activity $activity)
+    {
+        switch ($activity->subject_type) {
+            case \App\Task::class: return $this->item($activity->subject, new TaskTransformer);
+            case \App\Content::class: return $this->item($activity->subject, new ContentTrasnformer);
+            default: return null;
+        }
+    }
+}
