c4c2c32585437ee3199314fa959e9234d6d700f5
André Castelo <andrecastelo@gmail.com>
2017-06-09 13:43:11 -0300

    added code to register the account id on the activity log

--- app/Activity.php
+++ app/Activity.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Account;
 use App\Presenters\ActivityPresenter;
 use Illuminate\Database\Eloquent\Model;
 use Laracasts\Presenter\PresentableTrait;
@@ -11,4 +12,12 @@ class Activity extends \Spatie\Activitylog\Models\Activity
     use PresentableTrait;
 
     protected $presenter = ActivityPresenter::class;
+
+    public static function boot()
+    {
+        parent::boot();
+        static::creating(function($activity) {
+            $activity->account_id = Account::selectedAccount()->id;
+        });
+    }
 }
