5206e71eedfb1c3984653949fb4b0ff68be32822
André Castelo <andrecastelo@gmail.com>
2017-06-10 13:01:57 -0300

    added missing sections of the created dashboard

--- resources/assets/js/components/create/ContentDashboard.vue
+++ resources/assets/js/components/create/ContentDashboard.vue
@@ -9,8 +9,26 @@
             </h4>
 
             <content-list stage='published'></content-list>
-
         </div>
+
+        <div class="create-panel-container">
+            <h4 class="create-panel-heading">
+                <i class="icon-connect"></i>
+                READY TO PUBLISH
+            </h4>
+
+            <content-list stage='ready'></content-list>
+        </div>
+
+        <div class="create-panel-container">
+            <h4 class="create-panel-heading">
+                <i class="icon-connect"></i>
+                WRITTEN
+            </h4>
+
+            <content-list stage='written'></content-list>
+        </div>
+
     </div>
 </template>
 
