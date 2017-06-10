fef9915ba71d6e9ac0b3030d6f81f89fb85f2923
André Castelo <andrecastelo@gmail.com>
2017-06-10 13:28:22 -0300

    added some conditionals based on the content status

--- resources/assets/js/components/create/ContentListItem.vue
+++ resources/assets/js/components/create/ContentListItem.vue
@@ -14,27 +14,31 @@
         <div class="create-panel-table-cell text-center cell-size-5" :title="content.content_type">
             <i class="tooltip-icon large" :class='[ content.content_icon ]'></i>
         </div>
+
         <div class="create-panel-table-cell text-right cell-size-15">
-            <span class="dashboard-performing-text small">
+            <span class="dashboard-performing-text small" v-if='isPublished'>
                 UPDATED: <strong>{{ content.updated_at_format }}</strong>
             </span>
+
+            <span class="dashboard-performing-text small" :class="{ critical: content.due_date_critical }" v-if='isReadyToPublish'>
+                DUE: <strong>{{ content.due_date_format }}</strong>
+            </span>
         </div>
-        <!-- @can('guests-denied') -->
-        <!--
-        <div class="create-panel-table-cell text-right cell-size-10">
+
+        <div class="create-panel-table-cell text-right cell-size-10" v-if='!isGuest && isPublished'>
             <div class="create-dropdown">
                 <button type="button" class="button button-action" data-toggle="dropdown">
                     <i class="icon-add-circle"></i>
                 </button>
                 <ul class="dropdown-menu dropdown-menu-right">
                     <li>
-                        <a href="{{ route('archived_contents.update', $content) }}">Archive it</a>
+                        <a :href="archiveLink">Archive it</a>
                     </li>
                 </ul>
             </div>
         </div>
-        -->
-        <!-- @endcan -->
+
+
     </div>
 </template>
 
@@ -43,5 +47,23 @@
         name: 'content-list-item',
 
         props: ['content'],
+
+        computed: {
+            isGuest() {
+                return !!this.$store.state.user.is_guest;
+            },
+
+            isPublished() {
+                return this.content.content_status_id == 3;
+            },
+
+            isReadyToPublish() {
+                return this.content.content_status_id == 2;
+            },
+
+            archiveLink() {
+                return `/content/${this.content.id}/archive`;
+            }
+        }
     }
 </script>
