2cb95c0edd99df04a72c9c0151db690b3d0b8528
Travis Wilson <traviswilson@gmail.com>
2018-03-05 20:35:08 -0500

    Hide SEO on email, fb, twitter, linkedin

--- resources/assets/js/content/content_type_watcher.js
+++ resources/assets/js/content/content_type_watcher.js
@@ -3,35 +3,34 @@
 $(function() {
 
     const TYPE_BLOG_POST         = "1";
-    const TYPE_EMAIL             = "5";
     const TYPE_FACEBOOK_POST     = "2";
-    const TYPE_HUBSPOT_BLOG_POST = "4";
     const TYPE_TWEET             = "3";
+    const TYPE_HUBSPOT_BLOG_POST = "4";
+    const TYPE_EMAIL             = "5";
     const TYPE_WEBSITE_PAGE      = "6";
+    const TYPE_LINKEDIN          = "8";
+    const TYPE_CUSTOM            = "9";
 
     $('#contentType').change(refreshFields);
 
     function refreshFields() {
         const type = $('#contentType').val();
-        const showClass = (function() {
-            switch (type) {
-                case TYPE_EMAIL: return '.flexible-fields-email';
-                default:
-            }
-        })();
 
-        const hideClass = (function() {
-            switch(type) {
-                case TYPE_EMAIL: return '.flexible-fields-hide-email';
-                default:
-            }
-        })();
+        $('.flexible-fields-email').slideUp('fast');
 
-        $('.flexible-fields').slideUp('fast');
-        $('.flexible-fields-hide').slideDown('fast');
-
-        $(hideClass).slideUp('fast');
-        $(showClass).slideDown('fast');
+        switch (type) {
+            case TYPE_EMAIL:
+                $('.flexible-fields-email').slideDown('fast');
+                $('.flexible-fields-seo').slideUp('fast');
+                break;
+            case TYPE_FACEBOOK_POST:
+            case TYPE_TWEET:
+            case TYPE_LINKEDIN:
+                $('.flexible-fields-seo').slideUp('fast');
+                break;
+            default:
+                $('.flexible-fields-seo').slideDown('fast');
+        }
     }
 
     refreshFields();
